dc8c46faf747bef7a5fde2a4d36debcf8f5b6476
pachanga <none@none>
2007-04-09 13:13:46 +0400

    [svn r5576] -- using new TESTS_RUNNER ".ignore.php comes .init.php" schema
    --HG--
    branch : trunk

--- limb/tree/tests/cases/.ignore.php
+++ limb/tree/tests/cases/.ignore.php
@@ -1,4 +1,5 @@
 <?php
+require_once(dirname(__FILE__) . '/../../common.inc.php');
 return !file_exists(dirname(__FILE__) . '/../../init/init_tests.' .
                     lmbToolkit :: instance()->getDefaultDbConnection()->getType());
 ?>
