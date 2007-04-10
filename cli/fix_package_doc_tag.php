735de3cefa9554cb4a4a9441e722c5a46ac6968f
pachanga <none@none>
2007-04-10 20:56:41 +0400

    [svn r5612] -- improving fix_package_doc_tag.php
    --HG--
    branch : trunk

--- cli/fix_package_doc_tag.php
+++ cli/fix_package_doc_tag.php
@@ -1,5 +1,12 @@
 <?php
 set_time_limit(0);
+
+if($argc < 3)
+{
+  echo "Usage: fix_package_doc_tag <dir> <pkg>";
+  exit(1);
+}
+
 $dir = $argv[1];
 $pkg = $argv[2];
 $files = explode("\n", trim(`find $dir -type f -name "*.php"`));
@@ -8,7 +15,7 @@ foreach($files as $file)
 {
   echo "Processing $file...\n";
   $src = file_get_contents($file);
-  $src = preg_replace('~@package\s+(\?\?\?|\n)~', "@package    " . $pkg, $src);
+  $src = preg_replace('~@package\s+(\w+)~', "@package    " . $pkg, $src);
   file_put_contents($file, $src);
 }
 
