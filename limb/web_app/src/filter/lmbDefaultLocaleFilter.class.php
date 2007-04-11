cd0b23815d60b03591ac37da83ab9baf6a76111b
pachanga <none@none>
2007-04-11 17:28:12 +0400

    [svn r5636] -- lmbDefaultLocaleFilter fixed(WAP-4)
    --HG--
    branch : trunk

--- limb/web_app/src/filter/lmbDefaultLocaleFilter.class.php
+++ limb/web_app/src/filter/lmbDefaultLocaleFilter.class.php
@@ -6,7 +6,7 @@
  *
  * @copyright  Copyright &copy; 2004-2007 BIT
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
- * @version    $Id: lmbDefaultLocaleFilter.class.php 5012 2007-02-08 15:38:06Z pachanga $
+ * @version    $Id: lmbDefaultLocaleFilter.class.php 5636 2007-04-11 13:28:12Z pachanga $
  * @package    web_app
  */
 
@@ -25,19 +25,8 @@ class lmbDefaultLocaleFilter implements lmbInterceptingFilter
 
   function run($filter_chain)
   {
-    $this->_setDefaultLocale();
+    $this->toolkit->setLocale($this->default_locale);
     $filter_chain->next();
-  }
-
-  protected function _setDefaultLocale()
-  {
-    $this->_setLocaleByLocaleString($this->default_locale);
-  }
-
-  protected function _setLocaleByLocaleString($locale_string)
-  {
-    $locale = $this->toolkit->createLocale($locale_string);
-    $this->toolkit->setLocale($locale);
   }
 }
 
