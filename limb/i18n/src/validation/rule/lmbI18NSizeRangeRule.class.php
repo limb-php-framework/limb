4d71e4d6eea75ee4ad78afa61ecc16ebefece492
maximgb <none@none>
2007-04-12 15:21:34 +0400

    [svn r5650] -- Both {min} and {max} parameters are now accessable from the error message
    --HG--
    branch : trunk

--- limb/i18n/src/validation/rule/lmbI18NSizeRangeRule.class.php
+++ limb/i18n/src/validation/rule/lmbI18NSizeRangeRule.class.php
@@ -6,7 +6,7 @@
  *
  * @copyright  Copyright &copy; 2004-2007 BIT
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
- * @version    $Id: lmbI18NSizeRangeRule.class.php 5620 2007-04-11 09:04:24Z maximgb $
+ * @version    $Id: lmbI18NSizeRangeRule.class.php 5650 2007-04-12 11:21:34Z maximgb $
  * @package    i18n
  */
 lmb_require('limb/validation/src/rule/lmbSingleFieldRule.class.php');
@@ -42,14 +42,14 @@ class lmbI18NSizeRangeRule extends lmbSingleFieldRule
   {
     if(!is_null($this->min_length) && (lmb_strlen($value) < $this->min_length))
     {
-      $this->error(lmb_i18n('{Field} must be greater than {min} characters.', 'validation'),
-                   array('min' => $this->min_length));
+    	$this->error(lmb_i18n('{Field} must be greater than {min} characters.', 'validation'),
+                   array('min' => $this->min_length, 'max' => $this->max_length));
     }
 
     if(lmb_strlen($value) > $this->max_length)
     {
       $this->error(lmb_i18n('{Field} must be less than {max} characters.', 'validation'),
-                   array('max' => $this->max_length));
+                   array('min' => $this->min_length, 'max' => $this->max_length));
     }
   }
 }
