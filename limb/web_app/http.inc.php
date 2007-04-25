93d2cd601cf6d366493f82d82da609ab82e2e5ab
pachanga <none@none>
2007-04-25 14:20:30 +0400

    [svn r5773] -- a bit more optimal LIMB_HTTP* constant checks -- invalid declaration of LIMB_HTTP_OFFSET now yields exception
    --HG--
    branch : trunk

--- limb/web_app/http.inc.php
+++ limb/web_app/http.inc.php
@@ -24,29 +24,35 @@ else
 {
   $request = lmbToolkit :: instance()->getRequest();
 
-  @define('LIMB_HTTP_REQUEST_PATH', $request->getUri()->toString());
+  if(!defined('LIMB_HTTP_REQUEST_PATH'))
+    define('LIMB_HTTP_REQUEST_PATH', $request->getUri()->toString());
 
-  $offset = trim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
-  if($offset && $offset != '.')
-    @define('LIMB_HTTP_OFFSET_PATH', $offset . '/');
-  else
-    @define('LIMB_HTTP_OFFSET_PATH', '');
-
-  if(substr(LIMB_HTTP_OFFSET_PATH, 0, 1) == '/')
+  if(!defined('LIMB_HTTP_OFFSET_PATH'))
   {
-    echo('LIMB_HTTP_OFFSET_PATH constant must not have starting slash(' . LIMB_HTTP_OFFSET_PATH . ')!!!');
-    exit(1);
+    $offset = trim(dirname($_SERVER['SCRIPT_NAME']), '/\\');
+    if($offset && $offset != '.')
+      define('LIMB_HTTP_OFFSET_PATH', $offset . '/');
+    else
+      define('LIMB_HTTP_OFFSET_PATH', '');
   }
 
-  //HTTP_BASE_PATH is defined automatically according to current host and offset settings
-  @define('LIMB_HTTP_BASE_PATH', $request->getUri()->toString(array('protocol', 'user', 'password', 'host', 'port')) .
-                           '/' . LIMB_HTTP_OFFSET_PATH);
+  if(substr(LIMB_HTTP_OFFSET_PATH, 0, 1) == '/')
+    throw new lmbException('LIMB_HTTP_OFFSET_PATH constant must not have starting slash(' . LIMB_HTTP_OFFSET_PATH . ')!!!');
 
-  //check for mod_rewrite
-  if(defined('LIMB_ENABLE_MOD_REWRITE') && constant('LIMB_ENABLE_MOD_REWRITE'))
-    @define('LIMB_HTTP_GATEWAY_PATH', LIMB_HTTP_BASE_PATH);
-  else
-    @define('LIMB_HTTP_GATEWAY_PATH', LIMB_HTTP_BASE_PATH . 'index.php/');
+  //HTTP_BASE_PATH is defined automatically according to current host and offset settings
+  if(!defined('LIMB_HTTP_BASE_PATH'))
+  {
+    define('LIMB_HTTP_BASE_PATH', $request->getUri()->toString(array('protocol', 'user', 'password', 'host', 'port')) .
+                           '/' . LIMB_HTTP_OFFSET_PATH);
+  }
+
+  if(!defined('LIMB_HTTP_GATEWAY_PATH'))
+  {
+    if(defined('LIMB_ENABLE_MOD_REWRITE') && constant('LIMB_ENABLE_MOD_REWRITE'))
+      define('LIMB_HTTP_GATEWAY_PATH', LIMB_HTTP_BASE_PATH);
+    else
+      define('LIMB_HTTP_GATEWAY_PATH', LIMB_HTTP_BASE_PATH . 'index.php/');
+  }
 
   @define('LIMB_HTTP_SHARED_PATH', LIMB_HTTP_BASE_PATH . 'shared/');
 
