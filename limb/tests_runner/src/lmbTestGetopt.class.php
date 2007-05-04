55823077a9c18b28de9b76b732eb1bd5eed68d2f
tony <none@none>
2007-05-04 11:34:31 +0400

    [svn r5798] -- minor fixes for remove notice at options parsing
    --HG--
    branch : trunk

--- limb/tests_runner/src/lmbTestGetopt.class.php
+++ limb/tests_runner/src/lmbTestGetopt.class.php
@@ -229,7 +229,11 @@ class lmbTestGetopt {
 
             /* Check that the options uniquely matches one of the allowed
                options. */
-            $next_option_rest = substr($long_options[$i + 1], $opt_len);
+            $next_option_rest = '';
+
+            if(isset($long_options[$i + 1]))
+              $next_option_rest = substr($long_options[$i + 1], $opt_len);
+
             if ($opt_rest != '' && $opt{0} != '=' &&
                 $i + 1 < count($long_options) &&
                 $opt == substr($long_options[$i+1], 0, $opt_len) &&
