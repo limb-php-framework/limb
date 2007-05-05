fb4be36d6bad8424d986e08902451f4b185ee074
pachanga <none@none>
2007-05-05 17:41:55 +0400

    [svn r5816] -- self popup hrefs are skipped
    --HG--
    branch : trunk

--- limb/js/shared/limb/auto_popup.js
+++ limb/js/shared/limb/auto_popup.js
@@ -6,6 +6,10 @@ jQuery(window).ready(
     {
       if(this.href.indexOf('popup=1') > -1)
       {
+        //skip self hrefs with popup(basically it's <a href="#"> alike tags)
+        if(this.href.indexOf('#') > -1 && this.href.indexOf(window.location.href) > -1)
+          return;
+
         jQuery(this).bind("click", function()
                        {
                          new Limb.Window(this.href);
