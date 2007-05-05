1e0c248d064b00e0c4219d292a2e81facef8b912
pachanga <none@none>
2007-05-05 17:27:07 +0400

    [svn r5815] -- dumping old and junky calendar in favor of jscalendar-1.0(http://www.dynarch.com/demos/jscalendar/)
    --HG--
    branch : trunk

---
+++ limb/calendar/src/lmbCalendarWidget.class.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Limb Web Application Framework
+ *
+ * @link http://limb-project.com
+ *
+ * @copyright  Copyright &copy; 2004-2007 BIT
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ * @version    $Id$
+ * @package    calendar
+ */
+
+/**
+ *  This class implements a simple PHP wrapper for the calendar.  It
+ *  allows you to easily include all the calendar files and setup the
+ *  calendar by instantiating and calling a PHP object.
+ */
+class lmbCalendarWidget
+{
+  protected $newline = "\n";
+  protected $calendar_lib_path;
+
+  protected $calendar_file;
+  protected $calendar_lang_file;
+  protected $calendar_setup_file;
+  protected $calendar_theme_file;
+  protected $calendar_options;
+
+  function __construct($lang              = 'en',
+                       $theme             = 'calendar-win2k-1',
+                       $calendar_lib_path = '/shared/calendar/js/',
+                       $stripped          = true)
+  {
+    if($stripped)
+    {
+      $this->calendar_file = 'calendar_stripped.js';
+      $this->calendar_setup_file = 'calendar-setup_stripped.js';
+    }
+    else
+    {
+      $this->calendar_file = 'calendar.js';
+      $this->calendar_setup_file = 'calendar-setup.js';
+    }
+    $this->calendar_lang_file = 'lang/calendar-' . $lang . '.js';
+    $this->calendar_theme_file = $theme.'.css';
+    $this->calendar_lib_path = preg_replace('/\/+$/', '/', $calendar_lib_path);
+    $this->calendar_options = array('ifFormat' => '%Y-%m-%d',
+                                    'daFormat' => '%Y-%m-%d');
+  }
+
+  function setOption($name, $value)
+  {
+    $this->calendar_options[$name] = $value;
+  }
+
+  function loadFiles()
+  {
+    $code  = '<link rel="stylesheet" type="text/css" media="all" href="' .
+              $this->calendar_lib_path . $this->calendar_theme_file .
+               '" />' . $this->newline;
+    $code .=  '<script type="text/javascript" src="' .
+              $this->calendar_lib_path . $this->calendar_file .
+              '"></script>' . $this->newline;
+    $code .= '<script type="text/javascript" src="' .
+              $this->calendar_lib_path . $this->calendar_lang_file .
+              '"></script>' . $this->newline;
+    $code .= '<script type="text/javascript" src="' .
+             $this->calendar_lib_path . $this->calendar_setup_file .
+             '"></script>';
+    return $code;
+  }
+
+  function makeButton($field_id, $cal_options = array(), $field_attributes = array())
+  {
+    $id = $this->_genId();
+    $out = '<a href="#" id="'. $this->_triggerId($id) . '">' .
+        '<img align="middle" border="0" src="' . $this->calendar_lib_path . 'img.gif" alt="" /></a>';
+
+    $options = array_merge($cal_options,
+                           array('inputField' => $field_id,
+                                 'button'     => $this->_triggerId($id)));
+    return $out . $this->_makeCalendar($options);
+  }
+
+  function _makeCalendar($other_options = array())
+  {
+    $js_options = $this->_makeJsHash(array_merge($this->calendar_options, $other_options));
+    $code  = '<script type="text/javascript">Calendar.setup({' .
+             $js_options .
+             '});</script>';
+    return $code;
+  }
+
+  function _fieldId($id) { return 'f-calendar-field-' . $id; }
+  function _triggerId($id) { return 'f-calendar-trigger-' . $id; }
+  function _genId() { static $id = 0; return ++$id; }
+
+  function _makeJsHash($array)
+  {
+    $jstr = '';
+    reset($array);
+    while(list($key, $val) = each($array))
+    {
+      if(is_bool($val))
+        $val = $val ? 'true' : 'false';
+      else if(!is_numeric($val))
+        $val = '"'.$val.'"';
+      if($jstr) $jstr .= ',';
+      $jstr .= '"' . $key . '":' . $val;
+    }
+    return $jstr;
+  }
+
+  function _makeHtmlAttr($array)
+  {
+    $attrstr = '';
+    reset($array);
+    while(list($key, $val) = each($array))
+    {
+      $attrstr .= $key . '="' . $val . '" ';
+    }
+    return $attrstr;
+  }
+}
+
+?>
--- limb/calendar/src/template/tags/calendar.tag.php
+++ limb/calendar/src/template/tags/calendar.tag.php
@@ -6,10 +6,11 @@
  *
  * @copyright  Copyright &copy; 2004-2007 BIT
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
- * @version    $Id: calendar.tag.php 5796 2007-05-03 14:14:50Z pachanga $
+ * @version    $Id: calendar.tag.php 5815 2007-05-05 13:27:07Z pachanga $
  * @package    calendar
  */
 require_once('limb/wact/src/tags/form/input.tag.php');
+require_once('limb/calendar/src/lmbCalendarWidget.class.php');
 
 /**
 * @tag datetime,limb:CALENDAR
@@ -24,162 +25,29 @@ class lmbCalendarTag extends WactInputTag
 
   function prepare()
   {
-    $this->setAttribute('type', 'hidden');
-
-    $this->_ensureIdAttribute();
-
+    $this->setAttribute('type', 'text');
     parent :: prepare();
-  }
-
-  protected function _ensureIdAttribute()
-  {
-    if(!$this->hasAttribute('id'))
-      $this->setAttribute('id', $this->getServerId());
-  }
-
-  function preGenerate($code)
-  {
-    parent :: preGenerate($code);
-
-    $this->_createIframe($code);
   }
 
   function generateContents($code)
   {
-    $this->_createUIField($code);
-    $this->_createButton($code);
-    $this->_removeJunkyAttributes();
+    if(!$lang = $this->getAttribute('lang'))
+      $lang = 'en';
+
+    $widget = new lmbCalendarWidget($lang);
+
+    if($format = $this->getAttribute('format'))
+    {
+      $widget->setOption('ifFormat', $format);
+      $widget->setOption('daFormat', $format);
+    }
+
+    $code->writeHTML($widget->loadFiles() .
+                     $widget->makeButton($this->getAttribute('id')));
 
     parent :: generateContents($code);
   }
 
-  protected function _createIframe($code)
-  {
-    $date_iframe_id = $this->_getFrameId();
-    $id = $this->getServerId();
-
-    $code->writeHtml('<iframe tabindex="-1000" width=168 height=190 name="' . $date_iframe_id . '" id="' . $date_iframe_id . '"
-          src="' . LIMB_HTTP_SHARED_PATH . 'calendar/ipopeng.htm" scrolling="no"
-          frameborder="0" style="border:2px ridge; visibility:visible; z-index:999; position:absolute; left:-500px; top:0px;"' .
-          ' onload="init_calendar_' . $id . '()"></iframe>');
-  }
-
-  protected function _getFrameId()
-  {
-    //default-month:theme-name[:agenda-file[:context-name[:plugin-file]]]
-
-    $params = array();
-
-    if($default_month = $this->getAttribute('default_month'))
-    {
-      $params[0] = $default_month;
-      $this->removeAttribute('default_month');
-    }
-    else
-      $params[0] = 'gToday';
-
-    if($this->getAttribute('theme'))
-    {
-      $params[1] = $this->getAttribute('theme');
-      $this->removeAttribute('theme');
-    }
-    else
-      $params[1] = lmbToolkit :: instance()->getLocale();
-
-    if($this->getAttribute('agenda'))
-    {
-      $params[2] = $this->getAttribute('agenda');
-      $this->removeAttribute('agenda');
-    }
-    else
-      $params[2] = '';
-
-    if($this->getAttribute('context_name'))
-    {
-      $params[3] = $this->getAttribute('context_name');
-      $this->removeAttribute('context_name');
-    }
-    else
-      $params[3] = 'gfPop';
-
-    if($this->getAttribute('plugin'))
-    {
-      $params[4] = $this->getAttribute('plugin');
-      $this->removeAttribute('plugin');
-    }
-
-    return implode(':', $params);
-  }
-
-  protected function _getCalendarId()
-  {
-    if($id = $this->getAttribute('calendar_id'))
-      return $id;
-
-    return '_' . $this->getServerId();
-  }
-
-  protected function _createButton($code)
-  {
-    $parent = $this->findParentByClass('WactFormTag');
-    $id = $this->getServerId();
-    $form_id = $parent->getServerId();
-    $calendar_id = $this->_getCalendarId();
-
-    if(!$function = $this->getAttribute('function'))
-      $function = "fPopCalendar(document.getElementById(\"{$calendar_id}\"), document.getElementById(\"{$id}\"))";
-    else
-      $this->removeAttribute('function');
-
-    if(!$button = $this->getAttribute('button'))
-      $button = LIMB_HTTP_SHARED_PATH . "calendar/calbtn.gif";
-    else
-      $this->removeAttribute('button');
-
-    $code->writeHtml("<a href='javascript:void(0)' onclick='gfPop.". $function .";return false;' tabindex='-1000'>" .
-                     "<img name='popcal' align='absbottom' src='". $button ."' width='34' height='22' border='0' alt=''></a>");
-  }
-
-  function _createUIField($code)
-  {
-    $id = $this->getServerId();
-    $calendar_id = $this->_getCalendarId();
-    $attributes = '';
-    foreach($this->getAttributesAsArray() as $key => $value)
-    {
-      if($key == 'id' || $key == 'name' || $key == 'type')
-        continue;
-      $attributes .= "$key='$value' ";
-    }
-    $code->writeHtml("<input type='text' id='$calendar_id' $attributes />");
-    $code->writeHtml("<script>
-                     function init_calendar_$id()
-                     {
-                       var hidden=document.getElementById('$id');
-                       var input=document.getElementById('$calendar_id');
-                       input.value=gfPop.fStampToString(hidden.value * 1);
-
-                       var prev = function(){};
-                       if(hidden.form.onsubmit)
-                         prev = hidden.form.onsubmit;
-
-                       hidden.form.onsubmit = function() {
-                        hidden.value=gfPop.fStringToStamp(input.value);
-                        prev();
-                       };
-                     }
-                     </script>");
-  }
-
-  function _removeJunkyAttributes()
-  {
-    foreach($this->getAttributesAsArray() as $key => $value)
-    {
-      if($key == 'id' || $key == 'name' || $key == 'type')
-        continue;
-      $this->removeAttribute($key);
-    }
-  }
 }
 
 ?>
