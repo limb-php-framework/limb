dbb326435cfbbf44a2e93a99ccb1f7f7553aa6b1
pachanga <none@none>
2007-06-04 13:30:08 +0400

    [svn r5931] -- Limb.cookie(name, value, options) added(ported from jQuery cookie plugin) -- Limb.trim added, String.prototype.trim uses it
    --HG--
    branch : trunk

--- limb/js/shared/js/limb.js
+++ limb/js/shared/js/limb.js
@@ -5,19 +5,21 @@
  *
  * @copyright  Copyright &copy; 2004-2007 BIT
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
- * @version    $Id: limb.js 5897 2007-05-15 08:22:49Z pachanga $
+ * @version    $Id: limb.js 5931 2007-06-04 09:30:08Z pachanga $
  * @package    js
  */
 
 if(Limb == undefined) var Limb = {};
 
+Limb.trim = function(str)
+{
+  var r = /^\s+|\s+$/;
+  return str.replace(r,'');
+}
+
 if(!String.prototype.trim)
 {
-  String.prototype.trim = function()
-  {
-    var r=/^\s+|\s+$/;
-    return this.replace(r,'');
-  }
+  String.prototype.trim = Limb.trim;
 }
 
 if(!Function.prototype.bind)
@@ -149,6 +151,67 @@ Limb.isObject = function(variable)
 Limb.isFunction = function(variable)
 {
   return typeof(variable) == 'function';
+}
+
+/**
+ * Create a cookie with the given name and value and other optional parameters.
+ *
+ * @example Limb.cookie('the_cookie', 'the_value');
+ * @desc Set the value of a cookie.
+ * @example Limb.cookie('the_cookie', 'the_value', {expires: 7, path: '/', domain: 'jquery.com', secure: true});
+ * @desc Create a cookie with all available options.
+ * @example Limb.cookie('the_cookie', 'the_value');
+ * @desc Create a session cookie.
+ * @example Limb.cookie('the_cookie', null);
+ * @desc Delete a cookie by passing null as value.
+ */
+Limb.cookie = function(name, value, options)
+{
+  if(typeof value != 'undefined') // name and value given, set cookie
+  {
+    options = options || {};
+    if(value === null)
+    {
+      value = '';
+      options.expires = -1;
+    }
+    var expires = '';
+    if(options.expires && (typeof options.expires == 'number' || options.expires.toUTCString))
+    {
+        var date;
+        if(typeof options.expires == 'number')
+        {
+          date = new Date();
+          date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+        }
+        else
+          date = options.expires;
+        expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+    }
+    var path = options.path ? '; path=' + options.path : '';
+    var domain = options.domain ? '; domain=' + options.domain : '';
+    var secure = options.secure ? '; secure' : '';
+    document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+  }
+  else
+  { // only name given, get cookie
+    var cookieValue = null;
+    if(document.cookie && document.cookie != '')
+    {
+      var cookies = document.cookie.split(';');
+      for(var i = 0; i < cookies.length; i++)
+      {
+        var cookie = Limb.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if(cookie.substring(0, name.length + 1) == (name + '='))
+        {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
 }
 
 Limb.namespace('Limb.Classkit');
