4c76d2b0e41fcc851f5090bfbc31c32b62c91a41
wiliam <none@none>
2007-06-06 20:28:38 +0400

    [svn r5953] -- added some new extensions mime types
    --HG--
    branch : trunk

--- limb/net/src/lmbMimeType.class.php
+++ limb/net/src/lmbMimeType.class.php
@@ -2,43 +2,47 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 
 /**
  * class lmbMimeType.
  *
  * @package net
- * @version $Id: lmbMimeType.class.php 5945 2007-06-06 08:31:43Z pachanga $
+ * @version $Id: lmbMimeType.class.php 5953 2007-06-06 16:28:38Z wiliam $
  */
 class lmbMimeType
 {
   static protected $mime_types = array(
-    'doc' => 'application/msword',
-    'xls' => 'application/vnd.ms-excel',
-    'ppt' => 'application/vnd.ms-powerpoint',
-    'pdf' => 'application/pdf',
-    'zip' => 'application/zip',
-    'swf' => 'application/x-shockwave-flash',
-    'zip' => 'application/x-zip-compressed',
-    'wav' => 'audio/x-wav',
-    'mpeg' => 'audio/mpeg',
-    'bmp' => 'image/bmp',
-    'gif' => 'image/gif',
-    'jpg' => 'image/jpeg',
-    'jpeg' => 'image/pjpeg',
-    'png' => 'image/png',
-    'psd' => 'image/psd',
-    'msg' => 'message/rfc822',
-    'html' => 'text/html',
-    'txt' => 'text/plain',
-    'rtf' => 'text/rtf',
     'avi' => 'video/avi',
-    'mpg' => 'video/mpeg',
+    'aif' => 'audio/x-aiff',
+    'aifc' => 'audio/x-aiff',
+    'aiff' => 'audio/x-aiff',
+    'bmp' => 'image/bmp',
+    'doc' => 'application/msword',
+    'flv' => 'video/x-flv',
+    'gif' => 'image/gif',
+    'html' => 'text/html',
+    'jpeg' => 'image/pjpeg',
+    'jpg' => 'image/jpeg',
     'js' => 'text/javascript',
-    'flv' => 'video/x-flv'
+    'mpeg' => 'video/mpeg',
+    'mp3' => 'audio/mpeg',
+    'mpg' => 'video/mpeg',
+    'msg' => 'message/rfc822',
+    'pdf' => 'application/pdf',
+    'png' => 'image/png',
+    'ppt' => 'application/vnd.ms-powerpoint',
+    'psd' => 'image/psd',
+    'rtf' => 'text/rtf',
+    'swf' => 'application/x-shockwave-flash',
+    'txt' => 'text/plain',
+    'wav' => 'audio/x-wav',
+    'xls' => 'application/vnd.ms-excel',
+    'zip' => 'application/x-zip-compressed',
+    'zip' => 'application/zip',
   );
 
   static protected $flipped_mime_types = array();
