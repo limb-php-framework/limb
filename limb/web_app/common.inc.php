c4e5604e2817bf22a4dec60f2d660fa6eeb4cc41
pachanga <none@none>
2007-06-08 13:49:27 +0400

    [svn r5964] -- using lmb_require_optional
    --HG--
    branch : trunk

--- limb/web_app/common.inc.php
+++ limb/web_app/common.inc.php
@@ -2,19 +2,16 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 require_once('limb/core/common.inc.php');
 lmb_require('limb/view/wact.inc.php');
 lmb_require('limb/i18n/common.inc.php');
 
-if(lmb_is_readable('limb/dbal/common.inc.php'))
-  lmb_require('limb/dbal/common.inc.php');
-
-if(lmb_is_readable('limb/active_record/common.inc.php'))
-  lmb_require('limb/active_record/common.inc.php');
+lmb_require_optional('limb/dbal/common.inc.php');
+lmb_require_optional('limb/active_record/common.inc.php');
 
 lmb_require(dirname(__FILE__) . '/toolkit.inc.php');
 lmb_require(dirname(__FILE__) . '/http.inc.php');
