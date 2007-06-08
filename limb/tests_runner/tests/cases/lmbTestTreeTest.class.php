92f9fc8cb60dddbc7b6b28eafc357aa04a6922dc
pachanga <none@none>
2007-06-08 18:02:24 +0400

    [svn r5974] -- test_self.php now can optionally accept path to external SimpleTest library, very useful for testing new changes in SimpleTest -- ignoring MockTestGroup in lmbTestTreeTest, this raises issues with new mocks implementation in SimpleTest
    --HG--
    branch : trunk

--- limb/tests_runner/tests/cases/lmbTestTreeTest.class.php
+++ limb/tests_runner/tests/cases/lmbTestTreeTest.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 require_once(dirname(__FILE__) . '/../../src/lmbTestTree.class.php');
 require_once(dirname(__FILE__) . '/../../src/lmbTestTreeDirNode.class.php');
@@ -12,6 +12,8 @@ require_once(dirname(__FILE__) . '/../../src/lmbTestGroup.class.php');
 
 Mock :: generate('lmbTestTreeDirNode', 'MockTestTreeNode');
 Mock :: generate('lmbTestGroup', 'MockTestGroup');
+
+SimpleTest :: ignore('MockTestGroup');
 
 class lmbTestTreeTest extends UnitTestCase
 {
--- limb/tests_runner/tests/test_self.php
+++ limb/tests_runner/tests/test_self.php
@@ -2,13 +2,17 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
+
+if(isset($argv[1]))
+  define('SIMPLE_TEST', $argv[1]);
+
 require_once(dirname(__FILE__) . '/cases/.init.php');
 
-$group = new GroupTest();
+$group = new TestSuite();
 foreach(glob(dirname(__FILE__) . '/cases/*Test.class.php') as $file)
   $group->addTestFile($file);
 
