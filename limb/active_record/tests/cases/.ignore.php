9deb3fc3b18fcf4a6b2924318786caa4b4b10114
pachanga <none@none>
2007-06-09 16:34:40 +0400

    [svn r5980] -- friendly message added into .ignore.php
    --HG--
    branch : trunk

--- limb/active_record/tests/cases/.ignore.php
+++ limb/active_record/tests/cases/.ignore.php
@@ -1,6 +1,12 @@
 <?php
 require_once(dirname(__FILE__) . '/../../common.inc.php');
 
-return !file_exists(dirname(__FILE__) . '/.fixture/init_tests.' .
-                    lmbToolkit :: instance()->getDefaultDbConnection()->getType());
+$type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
+$ignore = !file_exists(dirname(__FILE__) . '/.fixture/init_tests.' . $type);
+$test_dir = basename(dirname(__FILE__));
+
+if($ignore)
+  echo "\nACTIVE_RECORD package '$test_dir' tests are skipped!(no compatible database fixture found for '$type' connection)\n\n";
+
+return $ignore;
 ?>
