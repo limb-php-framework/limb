9e8be08f06916d7cbef79a21d7204edf560bba2e
pachanga <none@none>
2007-06-15 14:55:31 +0400

    [svn r5995] -- better tests isolation in case when there's no compatible db test dump
    --HG--
    branch : trunk

--- limb/cms/tests/cases/.ignore.php
+++ limb/cms/tests/cases/.ignore.php
@@ -2,11 +2,11 @@
 require_once(dirname(__FILE__) . '/../../common.inc.php');
 
 $type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
-$ignore = !file_exists(dirname(__FILE__) . '/../../init/init_tests.' . $type);
+$ignore = !file_exists(dirname(__FILE__) . '/.fixture/init_tests.' . $type);
 $test_dir = basename(dirname(__FILE__));
 
 if($ignore)
-  echo "\nTREE package '$test_dir' tests are skipped!(no compatible database fixture found for '$type' connection)\n\n";
+  echo "\nCMS package '$test_dir' tests are skipped!(no compatible database fixture found for '$type' connection)\n\n";
 
 return $ignore;
 ?>
---
+++ limb/web_app/tests/cases/db/.ignore.php
@@ -0,0 +1,12 @@
+<?php
+require_once(dirname(__FILE__) . '/../../../common.inc.php');
+
+$type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
+$ignore = !file_exists(dirname(__FILE__) . '/.fixture.' . $type);
+$test_dir = basename(dirname(__FILE__));
+
+if($ignore)
+  echo "\nWEB_APP package '$test_dir' tests are skipped!(no compatible database fixture found for '$type' connection)\n\n";
+
+return $ignore;
+?>
