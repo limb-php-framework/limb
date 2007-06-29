4aa1ed8bf5146666dfa8a1dd2d40a88e9da5e81b
pachanga <none@none>
2007-06-29 13:05:54 +0400

    [svn r6026] -- fixing examples
    --HG--
    branch : trunk

--- limb/tests_runner/examples/runtests.php
+++ limb/tests_runner/examples/runtests.php
@@ -2,15 +2,17 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 
 require_once(dirname(__FILE__) . '/../common.inc.php');
 require_once(dirname(__FILE__) . '/../src/lmbTestRunner.class.php');
+require_once(dirname(__FILE__) . '/../src/lmbTestTreeFilePathNode.class.php');
 
-$ui = new lmbTestRunner(dirname(__FILE__) . '/cases/');
-exit($ui->run() ? 0 : 1);
+$ui = new lmbTestRunner();
+$res = $ui->run(new lmbTestTreeFilePathNode(dirname(__FILE__) . '/cases/'));
+exit($res ? 0 : 1);
 
 ?>
