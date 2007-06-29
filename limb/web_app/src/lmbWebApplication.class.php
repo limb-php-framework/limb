841ee598b224647fd3ce2c4a22b37bf321fdd211
pachanga <none@none>
2007-06-29 13:21:24 +0400

    [svn r6028] -- lmbErrorHandlingFilter added instead of lmbUncaughtExceptionHandlingFilter -- lmbUncaughtExceptionHandlingFilter extends lmbErrorHandlingFilter, it's deprecated and will be removed someday
    --HG--
    branch : trunk

--- limb/web_app/src/lmbWebApplication.class.php
+++ limb/web_app/src/lmbWebApplication.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 lmb_require('limb/filter_chain/src/lmbFilterChain.class.php');
 lmb_require('limb/core/src/lmbHandle.class.php');
@@ -13,13 +13,13 @@ lmb_require('limb/core/src/lmbHandle.class.php');
  * class lmbWebApplication.
  *
  * @package web_app
- * @version $Id: lmbWebApplication.class.php 5945 2007-06-06 08:31:43Z pachanga $
+ * @version $Id: lmbWebApplication.class.php 6028 2007-06-29 09:21:24Z pachanga $
  */
 class lmbWebApplication extends lmbFilterChain
 {
   function __construct()
   {
-    $this->registerFilter(new lmbHandle('limb/web_app/src/filter/lmbUncaughtExceptionHandlingFilter'));
+    $this->registerFilter(new lmbHandle('limb/web_app/src/filter/lmbErrorHandlingFilter'));
     $this->registerFilter(new lmbHandle('limb/web_app/src/filter/lmbSessionStartupFilter'));
     $this->registerFilter(new lmbHandle('limb/web_app/src/filter/lmbRequestDispatchingFilter',
                                         array(new lmbHandle('limb/web_app/src/request/lmbRoutesRequestDispatcher'))));
