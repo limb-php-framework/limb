0f635fcef0538adc6140c50ee0ceec38c7e2d1c7
pachanga <none@none>
2007-07-02 16:53:33 +0400

    [svn r6042] -- .ignore.php scripts fixes
    --HG--
    branch : trunk

--- limb/cms/tests/cases/.ignore.php
+++ limb/cms/tests/cases/.ignore.php
@@ -2,7 +2,7 @@
 require_once(dirname(__FILE__) . '/../../common.inc.php');
 
 $type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
-$ignore = !file_exists(dirname(__FILE__) . '/.fixture/init_tests.' . $type);
+$ignore = !file_exists(dirname(__FILE__) . '/.fixtures/init_tests.' . $type);
 $test_dir = basename(dirname(__FILE__));
 
 if($ignore)
