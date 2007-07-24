4ecfcda0cca0f7d9ddc658be92be57ffde590687
pachanga <none@none>
2007-07-24 13:23:15 +0400

    [svn r6192] -- mailing list test commit
    --HG--
    branch : trunk

--- limb/config/common.inc.php
+++ limb/config/common.inc.php
@@ -9,7 +9,7 @@
 
 /**
  * @package config
- * @version $Id: common.inc.php 5969 2007-06-08 10:51:09Z pachanga $
+ * @version $Id: common.inc.php 6192 2007-07-24 09:23:15Z pachanga $
  */
 require_once('limb/core/common.inc.php');
 require_once(dirname(__FILE__) . '/toolkit.inc.php');
