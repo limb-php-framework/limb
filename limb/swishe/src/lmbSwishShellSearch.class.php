bcf5aab9de61706cf6ba8d80a07cd573604f3307
pachanga <none@none>
2007-08-03 14:25:31 +0400

    [svn r6211] --adding missing header footers
    --HG--
    branch : trunk

--- limb/swishe/src/lmbSwishShellSearch.class.php
+++ limb/swishe/src/lmbSwishShellSearch.class.php
@@ -1,4 +1,16 @@
 <?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+/**
+ * @package swishe
+ * @version $Id$
+ */
 
 class lmbSwishShellSearch
 {
