f5002d6f49b194be6dd4f663bc223c1d1f399a72
tony <none@none>
2007-09-11 15:11:51 +0400

    [svn r6288] --added automatic js tabs initialization
    --HG--
    branch : trunk

--- limb/cms/shared/js/tabs.js
+++ limb/cms/shared/js/tabs.js
@@ -113,3 +113,31 @@ Limb.Class('CMS.Tab',
     this.link.onclick = this.onActivate.bind(this);
   }
 });
+
+if(jQuery)
+{
+  jQuery(window).ready(function()
+  {
+    autoTabs();
+  });
+
+  function autoTabs()
+  {
+    jQuery('.tabs').each(function(){
+      var tabs = [];
+
+      var ul = jQuery(this).children('ul');
+      ul.addClass('bookmarks');
+      ul.children('li').each(function(){
+        if(this.id)
+        {
+          jQuery('#tab_' + this.id).addClass('bookmarks_content');
+          tabs.push(new CMS.Tab(this.id, 'tab_' + this.id));
+        }
+      });
+
+      if(tabs.length)
+        new CMS.TabsContainer(tabs);
+    });
+  }
+}
