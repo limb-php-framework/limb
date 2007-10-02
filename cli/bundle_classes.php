0d9df92650b866a9938acbc0a61e99a3de2ef831
pachanga <none@none>
2007-10-03 01:01:37 +0400

    [svn r6365] -- non-root classes are listed first by bundle_classes.php
    --HG--
    branch : trunk

--- cli/bundle_classes.php
+++ cli/bundle_classes.php
@@ -2,7 +2,31 @@
 set_time_limit(0);
 
 $files = array();
-$classes = array();
+$root_classes = array();
+$nonroot_classes = array();
+
+function make_bundle($files, $filter_regex)
+{
+  $bundle = '';
+  foreach($files as $file)
+  {
+    $lines = file($file);
+    //removing <?php stuff
+    array_shift($lines); 
+    if(strpos($lines[count($lines)-1], '?>'))
+      array_pop($lines);
+    
+    //filter unneccessary lmb_require's
+    for($i=0;$i<count($lines);$i++)
+    {
+      if(preg_match($filter_regex, $lines[$i]))
+        unset($lines[$i]);
+    }
+
+    $bundle .= implode("", $lines);
+  }
+  return $bundle;
+}
 
 array_shift($argv);
 foreach($argv as $dir)
@@ -24,28 +48,23 @@ foreach($argv as $dir)
 $files = array_filter($files);
 $files = array_unique($files);
 
+$classes = array();
 foreach($files as $file)
-  $classes[] = reset(explode('.', basename($file))); 
+{
+  $class = reset(explode('.', basename($file))); 
+  if(preg_match("~class\s+$class\s+.*(extends|implements)~", file_get_contents($file)))
+    $nonroot_classes[] = $file;
+  else
+    $root_classes[] = $file;
+
+  $classes[] = $class;
+}
 
 $filter_regex = '~lmb_require.+(' . implode('|', $classes) . ')~';
 
 $bundle = '';
-foreach($files as $file)
-{
-  $lines = file($file);
-  //removing <?php stuff
-  array_shift($lines); 
-  if(strpos($lines[count($lines)-1], '?>'))
-    array_pop($lines);
-  
-  //filter unneccessary lmb_require's
-  for($i=0;$i<count($lines);$i++)
-  {
-    if(preg_match($filter_regex, $lines[$i]))
-      unset($lines[$i]);
-  }
 
-  $bundle .= implode("", $lines);
-}
+$bundle .= make_bundle($root_classes, $filter_regex);
+$bundle .= make_bundle($nonroot_classes, $filter_regex);
 
 echo "<?php\n" . $bundle;
