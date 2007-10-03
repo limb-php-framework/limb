9fd9b178dd7ecaced434280177198915d2ba7d6b
pachanga <none@none>
2007-10-04 00:13:11 +0400

    [svn r6375] -- adding benches for lmbObject attributes access
    --HG--
    branch : trunk

---
+++ limb/core/tests/bench/object-attr.php
@@ -0,0 +1,49 @@
+<?php
+
+set_include_path(dirname(__FILE__) . '/../../../../');
+require_once('limb/core/common.inc.php');
+require_once('limb/core/src/lmbObject.class.php');
+
+class Foo extends lmbObject
+{
+  function getBar()
+  {
+    return 'bar';
+  }
+}
+$object = new Foo(array('foo' => 'foo'));
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+  $object->get('foo');
+
+echo "raw getter access: " . (microtime(true) - $mark) . "\n";
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+  $object->get('bar');
+
+echo "raw getter access mapped to method: " . (microtime(true) - $mark) . "\n";
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+  $object->getBar();
+
+echo "static getter access: " . (microtime(true) - $mark) . "\n";
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+  $object->getFoo();
+
+echo "dynamic getter access: " . (microtime(true) - $mark) . "\n";
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+  $object->foo;
+
+echo "raw attribute access: " . (microtime(true) - $mark) . "\n";
