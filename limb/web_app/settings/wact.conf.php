cf544933c998e58cb5504439983c261e7434526f
pachanga <none@none>
2007-10-03 23:13:41 +0400

    [svn r6374] -- adding default macro.conf.php
    --HG--
    branch : trunk

---
+++ limb/web_app/settings/wact.conf.php
@@ -0,0 +1,8 @@
+<?php
+$conf = array(
+'forcescan' => false,  #Force to scan directories for tags, filters and properties (very slow)
+'forcecompile' => true #Force every template to be re-compiled on every request. Option is used
+                       #for debugging templates when developing template generation code
+
+);
+
