20a193496364d387af8e501e6855200caf0fb5b4
pachanga <none@none>
2007-10-04 00:24:39 +0400

    [svn r6376] -- adding bench for tools accessing from toolkit
    --HG--
    branch : trunk

---
+++ limb/toolkit/tests/bench/toolkit.php
@@ -0,0 +1,33 @@
+<?php
+
+set_include_path(dirname(__FILE__) . '/../../../../');
+require_once('limb/core/common.inc.php');
+require_once('limb/toolkit/src/lmbToolkit.class.php');
+require_once('limb/toolkit/src/lmbAbstractTools.class.php');
+
+class BenchTools extends lmbAbstractTools
+{
+  function getFoo()
+  {
+    return 'foo';
+  }
+}
+
+$toolkit = lmbToolkit :: setup(new BenchTools());
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+  $toolkit->getFoo();
+
+echo "tools method access: " . (microtime(true) - $mark) . "\n";
+
+$tools = new BenchTools();
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+  $tools->getFoo();
+
+echo "regular method access: " . (microtime(true) - $mark) . "\n";
+
