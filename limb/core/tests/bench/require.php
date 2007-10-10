7c079687c637c88cb4e2701473dc6717e5701afe
pachanga <none@none>
2007-10-10 22:46:01 +0400

    [svn r6411] -- better require benches
    --HG--
    branch : trunk

--- limb/core/tests/bench/require.php
+++ limb/core/tests/bench/require.php
@@ -1,9 +1,7 @@
 <?php
 
-set_include_path(dirname(__FILE__) . '/../../../../;.');
+set_include_path(dirname(__FILE__) . '/../../../../' . PATH_SEPARATOR. '.');
 require_once('limb/core/common.inc.php');
-
-//require_once(dirname(__FILE__) . '/bundle.inc.php');
 
 $path = dirname(__FILE__) . '/MyClass.class.php';
 
@@ -11,51 +9,120 @@ $mark = microtime(true);
 
 for($i=0;$i<1000;$i++)
 {
-  lmb_require(dirname(__FILE__) . '/MyClass' . $i . '.class.php');
+  lmb_require('Boo.class.php');
 }
 
-echo "lmb_require not included file: " . (microtime(true) - $mark) . "\n";
+echo "lmb_require same class, no autoload: " . (microtime(true) - $mark) . "\n";
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+{
+  lmb_require('Boo' . $i . '.class.php');
+}
+
+echo "lmb_require unique class, no autoload: " . (microtime(true) - $mark) . "\n";
 
 $mark = microtime(true);
 
 for($i=0;$i<1000;$i++)
 {
   lmb_require($path);
-  $object = new MyClass();
 }
+$object = new MyClass();
 
-echo "lmb_require absolute: " . (microtime(true) - $mark) . "\n";
+echo "lmb_require absolute, same class, autoload: " . (microtime(true) - $mark) . "\n";
 
 $mark = microtime(true);
 
 for($i=0;$i<1000;$i++)
 {
   lmb_require('MyClass.class.php');
-  $object = new MyClass();
+}
+$object = new MyClass();
+
+echo "lmb_require relative, same class, autoload: " . (microtime(true) - $mark) . "\n";
+
+for($i=0;$i<1000;$i++)
+{
+  make_class('UniqueClass' . $i);
 }
 
-echo "lmb_require relative: " . (microtime(true) - $mark) . "\n";
+$mark = microtime(true);
 
+for($i=0;$i<1000;$i++)
+{
+  $class = 'UniqueClass' . $i;
+  lmb_require(dirname(__FILE__) . '/tmp/'. $class . '.class.php');
+  $object = new $class;
+}
+
+echo "lmb_require absolute, unique class, autoload: " . (microtime(true) - $mark) . "\n";
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+{
+  $class = 'UniqueClass' . $i;
+  lmb_require(dirname(__FILE__) . '/tmp/'. $class . '.class.php');
+  $object = new $class;
+}
+
+echo "lmb_require absolute, again: " . (microtime(true) - $mark) . "\n";
 
 $mark = microtime(true);
 
 for($i=0;$i<1000;$i++)
 {
   require_once($path);
-  $object = new MyClass();
 }
+$object = new MyClass();
 
-echo "require once absolute: " . (microtime(true) - $mark) . "\n";
-
+echo "require_once absolute, same class: " . (microtime(true) - $mark) . "\n";
 
 $mark = microtime(true);
 
 for($i=0;$i<1000;$i++)
 {
   require_once('MyClass.class.php');
-  $object = new MyClass();
+}
+$object = new MyClass();
+
+echo "require_once relative, same class: " . (microtime(true) - $mark) . "\n";
+
+for($i=0;$i<1000;$i++)
+{
+  make_class('UniqueClazz' . $i);
 }
 
-echo "require once relative: " . (microtime(true) - $mark) . "\n";
+$mark = microtime(true);
 
+for($i=0;$i<1000;$i++)
+{
+  $class = 'UniqueClazz' . $i;
+  require_once(dirname(__FILE__) . '/tmp/' . $class . '.class.php');
+  $object = new $class();
+}
 
+echo "require_once absolute, unique class: " . (microtime(true) - $mark) . "\n";
+
+$mark = microtime(true);
+
+for($i=0;$i<1000;$i++)
+{
+  $class = 'UniqueClazz' . $i;
+  require_once(dirname(__FILE__) . '/tmp/' . $class . '.class.php');
+  $object = new $class();
+}
+
+echo "require_once absolute, again: " . (microtime(true) - $mark) . "\n";
+
+`rm -rf ./tmp`;
+
+function make_class($name)
+{
+  if(!is_dir('./tmp'))
+    mkdir('./tmp');
+  file_put_contents('./tmp/' . $name . '.class.php', 
+                    '<?php class ' . $name . ' {}; ?>');
+}
