d2c6cc76dec3e3cd9eae8e9e23d211aceac6d0f1
serega <none@none>
2007-10-19 12:11:46 +0400

    [svn r6442] -- better validation translations
    --HG--
    branch : trunk

--- limb/validation/i18n/translations/validation.ru_RU.ts
+++ limb/validation/i18n/translations/validation.ru_RU.ts
@@ -7,43 +7,43 @@
     </message>
     <message>
       <source>{Field} must contain only letters, numbers, hyphens, and periods.</source>
-      <translation type="unfinished"/>
+      <translation>Значение {Field} должно содержать только цифры, буквы, знаки тире и точки.</translation>
     </message>
     <message>
       <source>{Field} may not contain double hyphens (--).</source>
-      <translation type="unfinished"/>
+      <translation>не должно содержать двойного знака тире (--).</translation>
     </message>
     <message>
       <source>{Field} cannot start with a period.</source>
-      <translation type="unfinished"/>
+      <translation>Значение поля "{Field}" не должно начиняться с точки.</translation>
     </message>
     <message>
       <source>{Field} cannot end with a period.</source>
-      <translation type="unfinished"/>
+      <translation>Значение поля "{Field}" не должно заканчиваться точкой.</translation>
     </message>
     <message>
       <source>{Field} may not contain double periods (..).</source>
-      <translation type="unfinished"/>
+      <translation>не должно содержать двойных точек (..).</translation>
     </message>
     <message>
       <source>{Field} segment {segment} is too large (it must be 63 characters or less).</source>
-      <translation type="unfinished"/>
+      <translation>Сегмент {segment} поля "{Field}" слишком большой (должен быть менее 64 символов).</translation>
     </message>
     <message>
       <source>{Field} segment {segment} may not begin or end with a hyphen.</source>
-      <translation type="unfinished"/>
+      <translation>Сегмент {segment} поля "{Field}" не должен начинаться или заканчиваться знаком тире.</translation>
     </message>
     <message>
       <source>{Field} must contain a @ character.</source>
-      <translation type="unfinished"/>
+      <translation>Поле должно содержать знак @.</translation>
     </message>
     <message>
       <source>Invalid user in {Field}.</source>
-      <translation type="unfinished"/>
+      <translation>Неправильный  пользователь в поле "{Field}".</translation>
     </message>
     <message>
       <source>{Field} value is wrong</source>
-      <translation type="unfinished"/>
+      <translation>Значение поля "{Field}" не верно</translation>
     </message>
     <message>
       <source>{Field} - uploaded file was too large. Maximum size is {maxsize}.</source>
@@ -71,15 +71,15 @@
     </message>
     <message>
       <source>You have entered too many whole digits ({digits}) in {Field} (max {maxdigits}).</source>
-      <translation type="unfinished"/>
+      <translation>Вы ввели слишком много целых знаков ({digits}) в поле "{Field}" (max{maxdigits}).</translation>
     </message>
     <message>
       <source>You have entered too many decimal digits ({digits}) in {Field} (max {maxdigits}).</source>
-      <translation type="unfinished"/>
+      <translation>Вы ввели слишком много десятичных знаков ({digits}) в поле "{Field}" (max{maxdigits}).</translation>
     </message>
     <message>
       <source>{Field} must be a valid number.</source>
-      <translation type="unfinished"/>
+      <translation>Значение {Field} должно быть разрешенным числом.</translation>
     </message>
     <message>
       <source>{Field} must be not less than {value}.</source>
@@ -99,23 +99,23 @@
     </message>
     <message>
       <source>{Field} must be valid URL</source>
-      <translation type="unfinished"/>
+      <translation>{Field} должно быть правильным URL-адресом</translation>
     </message>
     <message>
       <source>{Field} must be greater than {min} characters.</source>
-      <translation type="unfinished"/>
+      <translation>Значение {Field}  должно быть больше {min} символов.</translation>
     </message>
     <message>
       <source>{Field} must be less than {max} characters.</source>
-      <translation type="unfinished"/>
+      <translation>Значение {Field}  должно быть меньше {max} символов.</translation>
     </message>
     <message>
       <source>Please specify a scheme for {Field}.</source>
-      <translation type="unfinished"/>
+      <translation>Пожалуйста, укажите протокол для поля "{Field}".</translation>
     </message>
     <message>
       <source>{Field} may not use {scheme}.</source>
-      <translation type="unfinished"/>
+      <translation>{Field} не может использоватеь протокол {scheme}.</translation>
     </message>
   </context>
 </TS>
