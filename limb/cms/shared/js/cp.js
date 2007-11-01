055f1d6d8ab3e0939aa8d8d9070245baa559c414
momental <none@none>
2007-11-01 10:56:41 +0300

    [svn r6477] -- improve styles
    --HG--
    branch : trunk

--- limb/cms/shared/js/cp.js
+++ limb/cms/shared/js/cp.js
@@ -1,4 +1,6 @@
 var LIMB_WINDOW_DEFAULT_PARAMS = { width: 890, height:500, resizable: true, noautoresize: true };
+var ShowFilterDefault = 'Показать фильтр';
+var HideFilterDefault = 'Скрыть фильтр';
 
 function toggle_selected(toggle_obj)
 {
@@ -22,8 +24,8 @@ Limb.Class('CMS.Filter',
     if (!filter.is('div'))
       return;
 
-    var activeFilterHTML = '<a class="active_filter"><span>Показать фильтр</span></a>';
-    var htmlText = '<div class="filter_bottom"><a class="active_filter_bottom"><span>Показать фильтр</span></a></div>';
+    var activeFilterHTML = '<a class="active_filter"><span>' + ShowFilterDefault + '</span></a>';
+    var htmlText = '<div class="filter_bottom"><a class="active_filter_bottom"><span>' + ShowFilterDefault + '</span></a></div>';
 
     filter.prepend(activeFilterHTML);
     list.css('margin','0');
@@ -35,13 +37,13 @@ Limb.Class('CMS.Filter',
 
     if(Limb.cookie(window.location + '.filter') == 1){
       filterForm.show();
-      this.activeFilter.text('Скрыть фильтр');
+      this.activeFilter.text(HideFilterDefault);
       this.activeFilter.addClass('show');
 
     }
     else {
       filterForm.hide();
-      this.activeFilter.text('Показать фильтр');
+      this.activeFilter.text(ShowFilterDefault);
     }
 
   },
@@ -53,11 +55,11 @@ Limb.Class('CMS.Filter',
 
     if (filterFormBelowList.is('form')){
       filterFormBelowList.hide();
-      filterFormBelowList.clone().appendTo(".filter").animate({height: 'show'}, "fast");
+      filterFormBelowList.clone().appendTo(".filter").show('slow');
       filterFormBelowList.remove();
     }
     else
-      filterForm.animate({height: 'toggle'}, "fast");
+      filterForm.toggle('slow');
 
     this.setFilterCookie();
     this.initActiveFilter();
@@ -69,10 +71,10 @@ Limb.Class('CMS.Filter',
     var filterFormBelowList = jQuery('.filter_bottom form');
 
     if (filterFormBelowList.is('form'))
-      filterFormBelowList.animate({height: 'toggle'}, "fast");
+      filterFormBelowList.toggle('slow');
     else {
       filterForm.hide();
-      filterForm.clone().prependTo(".filter_bottom").animate({height: 'show'}, "fast");
+      filterForm.clone().prependTo(".filter_bottom").show('slow');
       filterForm.remove();
     }
 
@@ -87,19 +89,19 @@ Limb.Class('CMS.Filter',
     if (filterForm.is('form')){
 
         if (this.activeFilter.attr('class')== 'show')
-          this.activeFilter.removeClass('show').text('Показать фильтр');
+          this.activeFilter.removeClass('show').text(ShowFilterDefault);
         else {
-          this.activeFilter.addClass('show').text('Скрыть фильтр');
-          this.activeFilterBelowList.removeClass('show').text('Показать фильтр');
+          this.activeFilter.addClass('show').text(HideFilterDefault);
+          this.activeFilterBelowList.removeClass('show').text(ShowFilterDefault);
         }
     }
     else{
 
         if (this.activeFilterBelowList.attr('class')== 'show')
-          this.activeFilterBelowList.removeClass('show').text('Показать фильтр');
+          this.activeFilterBelowList.removeClass('show').text(ShowFilterDefault);
         else {
-          this.activeFilterBelowList.addClass('show').text('Скрыть фильтр');
-          this.activeFilter.removeClass('show').text('Показать фильтр');
+          this.activeFilterBelowList.addClass('show').text(HideFilterDefault);
+          this.activeFilter.removeClass('show').text(ShowFilterDefault);
         }
 
     }
@@ -302,71 +304,162 @@ Limb.Class('CMS.UploadProgress',
   }
 });
 
+function initImgResize(){
+jQuery('img[@resize]')
+  .one('load', function()
+  {
+    var current_img = jQuery(this);
+    var size = current_img.attr('resize').replace('x','');
+    var width_bool = false;
+    var height_bool = false;
+    var width = current_img.width();
+    var height = current_img.height();
+
+    current_img.css({position:'absolute',top:'3000'});
+
+    if (current_img.attr('resize').indexOf('x') == 0)
+        height_bool = true;
+    else if (current_img.attr('resize').indexOf('x') == current_img.attr('resize').length - 1)
+      width_bool = true;
+    else if (current_img.attr('resize').indexOf('x') < 0)
+        if (width > height)
+          height_bool = true;
+        else
+          width_bool = true;
+
+    var link = jQuery('<a class="magnifier" href="' + this.src + '" target="_blank"><img src="/shared/cms/images/icon/magnifier.gif" width="13" height="13" alt="magnifier"/></a>').get()[0];
+
+    if(height_bool)
+    {
+      current_img.height(size);
+    }
+    else if (width_bool)
+    {
+      current_img.width(size);
+    }
+
+    jQuery(link).css('top', current_img.height() - 13);
+    jQuery(link).css('left', current_img.width() - 16);
+
+    current_img.css({position : 'static', top : '0'});
+    current_img.before(link);
+
+  });
+};
+
+Limb.Class('CMS.SidebarToggle',
+{
+  __construct:function()
+  {
+     this.sidebar = jQuery('#sidebar');
+     this.content = jQuery('#content');
+     this.bool = false;
+
+
+     var SidebarHTML = this.sidebar.html();
+     var toggleHTML = '<div class="sidebar_toggle"><span class="text">Навигация</span><a href="#" class="arrow"><img src="images/1x1.gif" width="8" height="7" alt="<<" /></a></div>';
+
+     this.sidebar.empty();
+     this.sidebar.prepend('<div class="inner"></div>');
+     this.inner = jQuery('#sidebar .inner');
+     this.inner.prepend(SidebarHTML);
+
+     this.sidebar.prepend(toggleHTML);
+     this.toggle = jQuery('.sidebar_toggle');
+     this.toggle_text = jQuery('.sidebar_toggle .text');
+     this.toggle_arrow = jQuery('.sidebar_toggle .arrow');
+
+     if(Limb.cookie('sidebar_toggle') == 1){
+       this.inner.hide();
+       this.toggle_text.hide();
+       this.hideSidebar();
+     }
+     this._initBehavior();
+  },
+
+  initSidebarStatus: function(){
+    if (this.sidebar.attr('class') == 'hide')
+       this.bool = true;
+     else
+       this.bool = false;
+     return this.bool;
+  },
+
+  showSidebar: function(){
+    this.inner.show('slow');
+    this.toggle_text.show('slow');
+    this.sidebar.removeClass('hide');
+    this.toggle.css('height','');
+    this.content.css('margin-left','225px');
+
+  },
+
+  hideSidebar: function(){
+    this.inner.hide('slow');
+    this.toggle_text.hide('slow');
+    this.sidebar.addClass('hide');
+    this.toggle.height(jQuery('body').height()-53);
+    this.sidebar.height(jQuery('body').height()-40);
+    this.content.css('margin-left','38px');
+  },
+
+
+  initToggleClick:function(){
+    if (this.initSidebarStatus())
+      this.showSidebar();
+    else
+      this.hideSidebar();
+
+    this.setToggleCookie();
+
+    return false;
+  },
+
+  setToggleCookie: function(){
+    if(Limb.cookie('sidebar_toggle') == 1)
+        Limb.cookie('sidebar_toggle', 0);
+    else
+      Limb.cookie('sidebar_toggle', 1);
+  },
+
+  _initBehavior: function(){
+    jQuery('.sidebar_toggle .arrow').click(this.initToggleClick.bind(this));
+  }
+
+});
+
+
+function initDocumentStructure(){
+  var container = jQuery('#container');
+  var sidebar = jQuery('#sidebar');
+  container.css({
+                position:'absolute',
+                overflow: 'auto',
+                top: '38px',
+                left: '0',
+                zIndex: 10
+  });
+  sidebar.css({
+                position:'absolute',
+                overflow: 'auto',
+                top: '38px',
+                left: '0',
+                zIndex: 20
+  });
+
+  var bodyHeight = jQuery('body').height()-40;
+  container.height(bodyHeight);
+  sidebar.height(bodyHeight);
+
+};
+
+
 function changed_field_highlighter()
 {
   jQuery(this).change(function(){jQuery(this).prev('label').css({color: 'green'})});
 }
 
-jQuery(window).ready(function(){
-
-  jQuery('.button').wrap('<span class="button_wrapper"></span>');
-
-  // Fiter up/down sliding control
-  new CMS.Filter('filter');
-  jQuery('.message_error .show_hidden').bind('click', control_error);
-
-  jQuery('img[@src$=.png], .shadow_bottom span, .shadow_left span, .shadow_right span').ifixpng()
-
-  //resized images
-  jQuery('img[@resize]')
-    .one('load', function()
-    {
-      var current_img = jQuery(this);
-      var size = current_img.attr('resize').replace('x','');
-      var width_bool = false;
-      var height_bool = false;
-      var width = current_img.width();
-      var height = current_img.height();
-
-      current_img.css({position:'absolute',top:'3000'});
-
-      if (current_img.attr('resize').indexOf('x') == 0)
-          height_bool = true;
-      else if (current_img.attr('resize').indexOf('x') == current_img.attr('resize').length - 1)
-        width_bool = true;
-      else if (current_img.attr('resize').indexOf('x') < 0)
-          if (width > height)
-            height_bool = true;
-          else
-            width_bool = true;
-
-      var link = jQuery('<a class="magnifier" href="' + this.src + '" target="_blank"><img src="/shared/cms/images/icon/magnifier.gif" width="13" height="13" alt="magnifier"/></a>').get()[0];
-
-      if(height_bool)
-      {
-        current_img.height(size);
-      }
-      else if (width_bool)
-      {
-        current_img.width(size);
-      }
-
-      jQuery(link).css('top', current_img.height() - 13);
-      jQuery(link).css('left', current_img.width() - 16);
-
-      current_img.css({position : 'static', top : '0'});
-      current_img.before(link);
-
-    });
-
-    //duplicating h1 popup in title
-    jQuery('.popup h1').each(
-      function()
-      {
-        document.title = jQuery(this).text();
-      }
-    );
-
+function initMainMenu(){
   //left navigation current item highlight
   var url = window.location.toString();
   var max = 0;
@@ -384,9 +477,44 @@ jQuery(window).ready(function(){
   if(link)
     jQuery(link).parent().attr('class', 'current');
 
+
   //sliding navigation support
   if(Limb.isFunction(jQuery.fn.accordion_cp))
     jQuery('#main_menu').accordion_cp();
+
+};
+
+
+/*WINDOW READY ==========================================*/
+jQuery(window).ready(function(){
+    initDocumentStructure();
+    jQuery(window).resize(initDocumentStructure);
+
+    jQuery('.button').wrap('<span class="button_wrapper"></span>');
+
+    new CMS.SidebarToggle('sidebar_toggle');
+
+    // Fiter up/down sliding control
+    new CMS.Filter('filter');
+
+    jQuery('.message_error .show_hidden').bind('click', control_error);
+
+    jQuery('img[@src$=.png], .shadow_bottom span, .shadow_left span, .shadow_right span').ifixpng();
+
+    initImgResize();
+
+    //resized images
+
+    //duplicating h1 popup in title
+    jQuery('.popup h1').each(
+      function()
+      {
+        document.title = jQuery(this).text();
+      }
+    );
+
+    initMainMenu();
+
 });
 
 Limb.namespace('rt.Util');
