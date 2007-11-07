6cb45c699ce95079634b44eb04e3cd0e9646efb4
serega <none@none>
2007-11-07 16:58:12 +0300

    [svn r6500] -- added test for new FallbackToViewController
    --HG--
    branch : trunk

---
+++ limb/web_app/tests/cases/plain/controller/FallbackToViewControllerTest.class.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+lmb_require('limb/web_app/src/controller/FallbackToViewController.class.php');
+lmb_require('limb/net/src/lmbHttpRequest.class.php');
+
+class FallbackToViewControllerTest extends UnitTestCase
+{
+  protected $toolkit;
+
+  function setUp()
+  {
+    $this->toolkit = lmbToolkit :: save();
+  }
+
+  function tearDown()
+  {
+    lmbToolkit :: restore();
+  }
+
+  function testAlwaysActionExists()
+  {
+    $controller = new FallbackToViewController();
+    $this->assertTrue($controller->actionExists('display'));
+  }
+
+  function testSetViewIfFoundAppropriateTemplate()
+  {
+    $this->toolkit->setSupportedViewTypes(array('.html' => 'lmbDummyView'));
+    $this->toolkit->setRequest(new lmbHttpRequest('http://localhost/about'));
+
+    $controller = new FallbackToViewController();
+    $controller->setCurrentAction('detail');
+
+    $controller->performAction();
+    $this->assertTrue($this->toolkit->getView()->getTemplate(), 'about.html');
+  }
+  
+  function testForwardTo404IfTemplateIsNotFound()
+  {
+    $view = new lmbDummyView();
+    $view->setTemplate('some_other_template.html');
+    $this->toolkit->setView($view);
+    $controller = new FallbackToViewController();
+    $controller->performAction();
+    $this->assertEqual($this->toolkit->getView()->getTemplate(), 'not_found.html');//too fragile check
+  }
+}
+
+
+
