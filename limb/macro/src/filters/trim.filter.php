0e9a9e3a992184bc49b2348851ee8dc3ed5e7e5a
serega <none@none>
2007-12-21 16:24:10 +0300

    [svn r6631] -- lmbMacroPhpFunctionBasedFilter moved to /compiler folder
    --HG--
    branch : trunk
    rename : limb/macro/src/filters/lmbMacroPhpFunctionBasedFilter.class.php => limb/macro/src/compiler/lmbMacroPhpFunctionBasedFilter.class.php

--- limb/macro/src/filters/trim.filter.php
+++ limb/macro/src/filters/trim.filter.php
@@ -6,7 +6,6 @@
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */ 
-lmb_require('limb/macro/src/filters/lmbMacroPhpFunctionBasedFilter.class.php');
  
 /**
  * class lmbMacroTrimFilter.
