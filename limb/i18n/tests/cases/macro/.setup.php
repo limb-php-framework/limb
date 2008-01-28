e0617e0489225e6747be7a1f6586759bb4e99fef
vasiatka <none@none>
2008-01-28 18:50:58 +0300

    [svn r6755] --Added i18n_date filter for macro templates --Added test for i18n_date filter for macro
    --HG--
    branch : trunk

---
+++ limb/i18n/tests/cases/macro/.setup.php
@@ -0,0 +1,10 @@
+<?php
+lmb_require('limb/macro/common.inc.php');
+lmb_require('limb/fs/src/lmbFs.class.php');
+lmb_require('limb/macro/src/lmbMacroTemplate.class.php');
+lmb_require('limb/macro/src/*.class.php');
+lmb_require('limb/macro/src/compiler/*.interface.php');
+lmb_require('limb/macro/src/compiler/*.class.php');
+lmb_require('limb/macro/tests/cases/lmbBaseMacroTest.class.php');
+lmb_require('limb/config/common.inc.php');
+
---
+++ limb/i18n/tests/cases/macro/lmbI18NDateMacroFilterTest.class.php
@@ -0,0 +1,151 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+lmb_require('limb/i18n/src/datetime/lmbLocaleDate.class.php');
+
+class lmbI18NDateMacroFilterTest extends lmbBaseMacroTest
+{
+  protected function _createMacro($file)
+  {
+    $base_dir = LIMB_VAR_DIR . '/tpl';
+    $cache_dir = LIMB_VAR_DIR . '/tpl/compiled';
+    $macro = new lmbMacroTemplate($file,
+                                  new lmbMacroConfig(
+                                                     $cache_dir,
+                                                     true,
+                                                     true,
+                                                     array($base_dir),
+                                                     null,
+                                                     array('limb/macro/src/filters','limb/i18n/src/macro')
+                                                     )
+                                 );
+    return $macro;
+  }
+
+  function tearDown()
+  {
+    lmbFs :: rm(LIMB_VAR_DIR . '/tpl');
+  }
+
+  function testSetDateByString()
+  {
+    $code = '{$#var|i18n_date:"en_US", "string"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+    $time = mktime(0, 0, 0, 2, 20, 2002);
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, '02/20/2002');
+  }
+
+  function testSetDateByStampValue()
+  {
+    $date = new lmbDateTime('2004-12-20 10:15:30');
+    $time=$date->getStamp();
+
+    $code = '{$#var|i18n_date:"en_US", "stamp"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, '12/20/2004');
+  }
+
+  function testFormatType()
+  {
+    $date = new lmbDateTime('2005-01-20 10:15:30');
+    $time=$date->getStamp();
+
+    $code = '{$#var|i18n_date:"en_US", "stamp", "date"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, 'Thursday 20 January 2005');
+  }
+
+  function testSetDateTimeByString()
+  {
+    $time='2002-02-20 10:23:24';
+
+    $code = '{$#var|i18n_date:"en_US", "string", "short_date_time"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, '02/20/2002 10:23:24');
+  }
+
+  function testDefinedFormat()
+  {
+    $date = new lmbDateTime('2004-12-20 10:15:30');
+    $time=$date->getStamp();
+
+    $code = '{$#var|i18n_date:"en_US", "stamp", "", "%Y %m %d"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, '2004 12 20');
+  }
+
+  function testUseRussianAsCurrentLocale()
+  {
+    $toolkit = lmbToolkit :: save();
+    $toolkit->addLocaleObject(new lmbLocale('ru_RU', new lmbIni(dirname(__FILE__).'/../../../i18n/locale/ru_RU.ini')));
+
+    $date = new lmbDateTime('2004-12-20 10:15:30');
+    $time=$date->getStamp();
+
+    $code = '{$#var|i18n_date:"ru_RU", "stamp"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, '20.12.2004');
+
+    lmbToolkit :: restore();
+  }
+
+  function testComplexPathBasedDBEWithDefinedFormat()
+  {
+    $date = new lmbDateTime('2005-01-20 10:15:30');
+    $my_dataspace = new lmbSet(array('var' => $date->getStamp()));
+
+    $code = '{$#my.var|i18n_date:"en_US", "stamp", "", "%Y %m %d"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+
+    $tpl->set('my', $my_dataspace);
+    $out = $tpl->render();
+    $this->assertEqual($out, '2005 01 20');
+  }
+
+  function testDateByCurrentLocale()
+  {
+    $date = new lmbDateTime('2004-12-20 10:15:30');
+    $time=$date->getStamp();
+
+  	$code = '{$#var|i18n_date:"","stamp"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, '12/20/2004');
+  }
+
+  function testWithOutParams()
+  {
+    $date = new lmbDateTime('2004-12-20 10:15:30');
+    $time=$date->getStamp();
+
+  	$code = '{$#var|i18n_date}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+    $tpl->set('var', $time);
+    $out = $tpl->render();
+    $this->assertEqual($out, '12/20/2004');
+  }
+}
+
