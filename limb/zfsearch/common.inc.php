8b723d67a44dad76de6844ea627ede8302d0a361
pachanga <none@none>
2008-03-18 13:52:10 +0300

    [svn r6841] -- adding inclusion of Zend_Search_Lucene_Document_Html
    --HG--
    branch : trunk

--- limb/zfsearch/common.inc.php
+++ limb/zfsearch/common.inc.php
@@ -14,6 +14,7 @@
 lmb_require('limb/i18n/utf8.inc.php');
 set_include_path(get_include_path() . dirname(__FILE__).'/lib/');
 require_once('Zend/Search/Lucene.php');
+require_once('Zend/Search/Lucene/Document/Html.php');
 @define('ZEND_SEARCH_ENCODING','utf-8');
 Zend_Search_Lucene_Analysis_Analyzer::setDefault(new Zend_Search_Lucene_Analysis_Analyzer_Common_Utf8());
 
