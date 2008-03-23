a25043f427bb8dcd93b68cc90b6198cf0b2ddb00
pachanga <none@none>
2008-03-23 19:36:54 +0300

    [svn r6851] -- adding convinience Limb3 packages tests running script
    --HG--
    branch : trunk

---
+++ limb/runtests.php
@@ -0,0 +1,29 @@
+<?php
+set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
+
+require_once(dirname(__FILE__) . '/tests_runner/common.inc.php');
+require_once(dirname(__FILE__) . '/tests_runner/src/lmbTestRunner.class.php');
+require_once(dirname(__FILE__) . '/tests_runner/src/lmbTestTreeGlobNode.class.php');
+
+$packages = null;
+if(sizeof($argv) > 1)
+  $packages = array_splice($argv, 1);
+
+
+if($packages)
+{
+  $paths = array();
+  $res = true;
+  foreach($packages as $pkg)
+  {
+    $runner = new lmbTestRunner();
+    $res &= $runner->run(new lmbTestTreeDirNode(dirname(__FILE__) . '/' . $pkg . '/tests/cases/'));
+  }
+}
+else
+{
+  $runner = new lmbTestRunner();
+  $res = $runner->run(new lmbTestTreeGlobNode(dirname(__FILE__) . '/*/tests/cases/'));
+}
+
+exit($res ? 0 : 1);
