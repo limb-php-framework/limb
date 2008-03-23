78d1937485bb90df400edbeb12a4267528424d08
pachanga <none@none>
2008-03-23 19:39:18 +0300

    [svn r6852] -- defining LIMB_VAR_DIR and automatically creating var directory
    --HG--
    branch : trunk

--- limb/core/tests/cases/.setup.php
+++ limb/core/tests/cases/.setup.php
@@ -1,5 +1,10 @@
 <?php
 
-require_once(dirname(__FILE__) . '/../../common.inc.php');
+if(!defined('LIMB_VAR_DIR'))
+{
+  @define('LIMB_VAR_DIR', dirname(__FILE__) . '/../../../var');
+  @mkdir(LIMB_VAR_DIR);
+}
 
+require_once(dirname(__FILE__) . '/../../common.inc.php');
 
--- limb/macro/tests/cases/.setup.php
+++ limb/macro/tests/cases/.setup.php
@@ -1,4 +1,10 @@
 <?php
+if(!defined('LIMB_VAR_DIR'))
+{
+  @define('LIMB_VAR_DIR', dirname(__FILE__) . '/../../../var');
+  @mkdir(LIMB_VAR_DIR);
+}
+
 require_once(dirname(__FILE__) . '/../../common.inc.php');
 
 lmb_require('limb/fs/src/lmbFs.class.php');
