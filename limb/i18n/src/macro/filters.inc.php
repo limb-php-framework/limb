41bed1742eb361ba77c11676d85d9fc900a83317
vasiatka <none@none>
2008-03-26 19:42:58 +0300

    [svn r6859] -- i18n_filter for macro was changed, filter function was placed into file filters.inc.php
    --HG--
    branch : trunk

---
+++ limb/i18n/src/macro/filters.inc.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+require_once('limb/i18n/src/datetime/lmbLocaleDateTime.class.php');
+require_once('limb/datetime/src/lmbDateTime.class.php');
+
+function lmb_i18n_date_filter($params,$value)
+{
+  $toolkit = lmbToolkit :: instance();
+  if(isset($params[0]) && $params[0])
+  {
+    $locale=$toolkit->getLocaleObject($params[0]);
+  }
+  else
+    $locale=$toolkit->getLocaleObject();
+
+  if(isset($params[3]) && $params[3])
+    $format=$params[3];
+  else
+  {
+    if(isset($params[2]) && $params[2])
+      $format_type = $params[2];
+    else
+      $format_type = 'short_date';
+
+    $property = $format_type . '_format';
+    $format=$locale->$property;
+  }
+
+  if(isset($params[1]) && $params[1])
+    $date_type = $params[1];
+  else
+    $date_type = 'stamp';
+
+  switch($date_type)
+  {
+    case 'string': $date = new lmbLocaleDateTime($value);      break;
+    case 'stamp':  $date = new lmbLocaleDateTime((int)$value); break;
+    default:       $date = new lmbLocaleDateTime($value);      break;
+  }
+
+  return $date->localeStrftime($format, $locale);
+}
--- limb/i18n/src/macro/i18n_date.filter.php
+++ limb/i18n/src/macro/i18n_date.filter.php
@@ -7,8 +7,6 @@
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 
-lmb_require('limb/i18n/src/datetime/lmbLocaleDateTime.class.php');
-
 /**
  * Filter i18n_date for macro templates
  * @filter i18n_date
@@ -21,54 +19,17 @@ class lmbI18NMacroDateFilter extends lmbMacroFilter
 
   function preGenerate($code)
   {
-    $code->registerInclude('limb/i18n/src/datetime/lmbLocaleDateTime.class.php');
-    $code->registerInclude('limb/datetime/src/lmbDateTime.class.php');
+    $code->registerInclude('limb/i18n/src/macro/filters.inc.php');
     parent :: preGenerate($code);
   }
   function getValue()
   {
     $params="array(";
     foreach ($this->params as $key=>$value)
-    {      $params.=$value.",";    }
+    {
+      $params.=$value.",";
+    }
     $params.=")";
-    return 'lmbI18NMacroDateFilter::_i18nDateFilter(' . $params.', ' . $this->base->getValue() . ')';
-  }
-
-  static function _i18nDateFilter($params,$value)
-  {
-    $toolkit = lmbToolkit :: instance();
-    if(isset($params[0]) && $params[0])
-    {
-      $locale=$toolkit->getLocaleObject($params[0]);
-    }
-    else
-      $locale=$toolkit->getLocaleObject();
-
-    if(isset($params[3]) && $params[3])
-      $format=$params[3];
-    else
-    {      if(isset($params[2]) && $params[2])
-        $format_type = $params[2];
-      else
-        $format_type = 'short_date';
-
-      $property = $format_type . '_format';
-      $format=$locale->$property;
-    }
-
-    if(isset($params[1]) && $params[1])
-      $date_type = $params[1];
-    else
-      $date_type = 'stamp';
-
-    switch($date_type)
-    {
-      case 'string': $date = new lmbLocaleDateTime($value);      break;
-      case 'stamp':  $date = new lmbLocaleDateTime((int)$value); break;
-      default:       $date = new lmbLocaleDateTime($value);      break;
-    }
-
-    return $date->localeStrftime($format, $locale);
+    return 'lmb_i18n_date_filter(' . $params.', ' . $this->base->getValue() . ')';
   }
 }
-
