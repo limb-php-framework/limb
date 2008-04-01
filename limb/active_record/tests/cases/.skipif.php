7ec86868f2a7251f6dec9671c29404e94b5dc90e
pachanga <none@none>
2008-04-02 00:32:33 +0400

    [svn r6887] -- fixing active_record tests fixtures
    --HG--
    branch : trunk

--- limb/active_record/tests/cases/.skipif.php
+++ limb/active_record/tests/cases/.skipif.php
@@ -1,5 +1,19 @@
 <?php
+if(!defined('LIMB_VAR_DIR'))
+{
+  @define('LIMB_VAR_DIR', dirname(__FILE__) . '/../../../var');
+  if(!is_dir(LIMB_VAR_DIR) && !mkdir(LIMB_VAR_DIR))
+    throw new Exception("Could not create LIMB_VAR_DIR at '" . LIMB_VAR_DIR . "' during tests execution");
+}
+
 require_once(dirname(__FILE__) . '/../../common.inc.php');
+
+if(!lmbToolkit::instance()->isDefaultDbDSNAvailable())
+{
+  $dsn = 'sqlite://localhost/' . LIMB_VAR_DIR . '/sqlite_tests.db';
+  echo "Using default sqlite test database '$dsn'\n";
+  lmbToolkit::instance()->setDefaultDbDSN($dsn);
+}
 
 $type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
 $skip = !file_exists(dirname(__FILE__) . '/.fixture/init_tests.' . $type);
