a222fb79f525cbacfaa5e312d60130ed4c4231f3
pachanga <none@none>
2008-04-01 22:32:26 +0400

    [svn r6883] -- isolating tree tests
    --HG--
    branch : trunk

--- limb/tree/tests/cases/.setup.php
+++ limb/tree/tests/cases/.setup.php
@@ -1,7 +1,25 @@
 <?php
+if(!defined('LIMB_VAR_DIR'))
+{
+  @define('LIMB_VAR_DIR', dirname(__FILE__) . '/../../../var');
+  if(!is_dir(LIMB_VAR_DIR) && !mkdir(LIMB_VAR_DIR))
+    throw new Exception("Could not create LIMB_VAR_DIR at '" . LIMB_VAR_DIR . "' during tests execution");
+}
+
 require_once(dirname(__FILE__) . '/../../common.inc.php');
 require_once('limb/dbal/src/lmbDbDump.class.php');
-$type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
-$this->dump = new lmbDbDump(dirname(__FILE__) . '/../../init/init_tests.' . $type);
-$this->dump->load();
 
+if(!lmbToolkit::instance()->isDefaultDbDsnAvailable())
+{
+  $dsn = 'sqlite://localhost/' . LIMB_VAR_DIR . '/sqlite_tests.db';
+  echo "Using default sqlite test database '$dsn'\n";
+  lmbToolkit::instance()->setDefaultDbDSN($dsn);
+}
+
+$type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
+$file = dirname(__FILE__) . '/../../init/init_tests.' . $type;
+if(file_exists($file))
+{
+  $this->dump = new lmbDbDump($file);
+  $this->dump->load();
+}
