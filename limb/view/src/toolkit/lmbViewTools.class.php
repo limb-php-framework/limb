3d7fc4c33644b2fef3e23189a8c6ae468f75d0a4
pachanga <none@none>
2008-04-09 23:43:48 +0400

    [svn r6906] -- LIMB_SUPPORTED_VIEW_TYPES constant added, it can be used to setup which view types should be used
    --HG--
    branch : trunk

--- limb/view/src/toolkit/lmbViewTools.class.php
+++ limb/view/src/toolkit/lmbViewTools.class.php
@@ -12,6 +12,7 @@ lmb_require('limb/toolkit/src/lmbAbstractTools.class.php');
 @define('LIMB_WACT_TAGS_INCLUDE_PATH', 'src/wact;limb/*/src/wact;limb/wact/src/tags;src/template/tags;limb/*/src/template/tags;limb/wact/src/tags');
 @define('LIMB_MACRO_TAGS_INCLUDE_PATH', 'src/macro;limb/*/src/macro;limb/macro/src/tags');
 @define('LIMB_MACRO_FILTERS_INCLUDE_PATH', 'src/macro;limb/*/src/macro;limb/macro/src/filters');
+@define('LIMB_SUPPORTED_VIEW_TYPES', '.html=lmbWactView;.phtml=lmbMacroView');
 
 /**
  * class lmbViewTools.
@@ -21,11 +22,22 @@ lmb_require('limb/toolkit/src/lmbAbstractTools.class.php');
  */
 class lmbViewTools extends lmbAbstractTools
 {
-  protected $view_types = array('.html' => 'lmbWactView',
-                                '.phtml' => 'lmbMacroView');
+  protected $view_types = array();
   protected $wact_locator;
   protected $macro_config;
   protected $macro_locator;
+
+  function __construct()
+  {
+    parent :: __construct();
+
+    $items = explode(';', LIMB_SUPPORTED_VIEW_TYPES);
+    foreach($items as $item)
+    {
+      list($ext, $class) = explode('=', $item);
+      $this->view_types[$ext] = $class;
+    }
+  }
 
   function setSupportedViewTypes($types)
   {
