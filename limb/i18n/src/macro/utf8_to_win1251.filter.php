aaeb57cb4380f0c9ebc157d4675ce8ac9dbe0434
pachanga <none@none>
2008-05-21 09:15:19 +0400

    [svn r7032] -- renaming filter to utf8_to_win1251
    --HG--
    branch : trunk
    rename : limb/i18n/src/macro/utf_to_win1251.filter.php => limb/i18n/src/macro/utf8_to_win1251.filter.php

---
+++ limb/i18n/src/macro/utf8_to_win1251.filter.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @filter utf8_to_win1251
+ */
+class Utf8ToWin1251 extends lmbMacroFunctionBasedFilter
+{
+  protected $function = 'lmb_utf8_to_win1251';
+  protected $include_file = 'limb/i18n/utf8.inc.php';
+}
