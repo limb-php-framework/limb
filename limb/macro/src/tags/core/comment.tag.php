a8042c7be396b06c792a750e89fae79de0b45750
cmz <none@none>
2008-06-02 22:32:45 +0400

    [svn r7048] -- added macro-tag {{comment}} (alias {{*}})
    --HG--
    branch : trunk

---
+++ limb/macro/src/tags/core/comment.tag.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2008 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+/**
+ * class lmbMacroCommentTag.
+ * @tag comment
+ * @aliases *
+ * @package macro
+ * @version $Id: comment.tag.php 7048 2008-06-02 18:32:45Z cmz $
+ */
+class lmbMacroCommentTag extends lmbMacroPassiveTag
+{
+}
