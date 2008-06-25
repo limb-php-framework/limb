4f3f973cfa9b0e6839cee4ad9704bc1740bc2fb9
korchasa <none@none>
2008-06-25 16:00:59 +0400

    [svn r7065] -- remove include loop with common.inc.php
    --HG--
    branch : trunk

--- limb/dbal/src/lmbDBAL.class.php
+++ limb/dbal/src/lmbDBAL.class.php
@@ -6,7 +6,6 @@
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
-require_once('limb/dbal/common.inc.php');
 lmb_require('limb/dbal/src/lmbSimpleDb.class.php');
 lmb_require('limb/dbal/src/query/lmbSelectQuery.class.php');
 lmb_require('limb/dbal/src/query/lmbUpdateQuery.class.php');
@@ -16,10 +15,13 @@ lmb_require('limb/dbal/src/query/lmbDeleteQuery.class.php');
  * class lmbDBAL.
  *
  * @package dbal
- * @version $Id: lmbDBAL.class.php 6930 2008-04-14 11:22:49Z pachanga $
+ * @version $Id: lmbDBAL.class.php 7065 2008-06-25 12:00:59Z korchasa $
  */
 class lmbDBAL
 {
+  /**
+   * @param lmbDbDSN $dsn
+   */
   static function setDefaultDSN($dsn)
   {
     lmbToolkit :: instance()->setDefaultDbDSN($dsn);
@@ -30,16 +32,29 @@ class lmbDBAL
     lmbToolkit :: instance()->setDbEnvironment($env);
   }
 
+  /**
+   * @param lmbDbDSN $dsn
+   * @return lmbDbConnetion
+   */
   static function newConnection($dsn)
   {
     return lmbToolkit :: instance()->createDbConnection($dsn);
   }
 
+  /**
+   * @param lmbDbDSN $dsn
+   * @return lmbDbConnetion
+   */
   static function defaultConnection()
   {
     return lmbToolkit :: instance()->getDefaultDbConnection();
   }
 
+  /**
+   * @param string $sql
+   * @param [lmbDbConnection] $conn
+   * @return lmbDbStatement
+   */
   static function newStatement($sql, $conn = null)
   {
     if(!$conn)
@@ -47,6 +62,10 @@ class lmbDBAL
     return $conn->newStatement($sql);
   }
 
+  /**
+   * @param [lmbDbConnetion] $conn
+   * @return lmbSimpleDb
+   */
   static function db($conn = null)
   {
     if(!$conn)
@@ -65,21 +84,41 @@ class lmbDBAL
     return lmbToolkit :: instance()->createTableGateway($table, $conn);
   }
 
+  /**
+   * @param string $table
+   * @param [lmbDbConnection] $conn
+   * @return lmbSelectQuery
+   */
   static function selectQuery($table, $conn = null)
   {
     return new lmbSelectQuery($table, $conn);
   }
 
+  /**
+   * @param string $table
+   * @param [lmbDbConnection] $conn
+   * @return lmbUpdateQuery
+   */
   static function updateQuery($table, $conn = null)
   {
     return new lmbUpdateQuery($table, $conn);
   }
 
+  /**
+   * @param string $table
+   * @param [lmbDbConnection] $conn
+   * @return lmbDeleteQuery
+   */
   static function deleteQuery($table, $conn = null)
   {
     return new lmbDeleteQuery($table, $conn);
   }
 
+  /**
+   * @param string $sql
+   * @param [lmbDbConnection] $conn
+   * @return lmbDbRecordSet
+   */
   static function fetch($sql, $conn = null)
   {
     if(!$conn)
@@ -88,6 +127,22 @@ class lmbDBAL
     return $stmt->getRecordSet();
   }
 
+  /**
+   * @param string $sql
+   * @param [lmbDbConnection] $conn
+   * @return string
+   */
+  static function fetchOneValue($sql, $conn = null)
+  {
+    if(!$conn)
+      $conn = lmbToolkit :: instance()->getDefaultDbConnection();
+    return $conn->newStatement($sql)->getOneValue();
+  }
+
+  /**
+   * @param string $sql
+   * @param [lmbDbConnection] $conn
+   */
   static function execute($sql, $conn = null)
   {
     if(!$conn)
