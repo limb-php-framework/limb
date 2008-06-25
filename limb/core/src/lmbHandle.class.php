2aa113cebf0e089ac57000516a2f0a8ded29edc6
korchasa <none@none>
2008-06-25 18:53:21 +0400

    [svn r7074] -- fix relative path in core package
    --HG--
    branch : trunk

--- limb/core/src/lmbHandle.class.php
+++ limb/core/src/lmbHandle.class.php
@@ -2,11 +2,11 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
-lmb_require(dirname(__FILE__) . '/lmbProxy.class.php');
+lmb_require('/limb/core/scr/lmbProxy.class.php');
 
 /**
  * class lmbHandle.
