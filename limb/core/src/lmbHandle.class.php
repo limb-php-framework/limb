ce36c1d2461e596960daee77f0b6403eb5c461ce
korchasa <none@none>
2008-06-25 20:20:21 +0400

    [svn r7078] -- typo fix
    --HG--
    branch : trunk

--- limb/core/src/lmbHandle.class.php
+++ limb/core/src/lmbHandle.class.php
@@ -6,7 +6,7 @@
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
-lmb_require('limb/core/scr/lmbProxy.class.php');
+lmb_require('limb/core/src/lmbProxy.class.php');
 
 /**
  * class lmbHandle.
