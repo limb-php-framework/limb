52b2a8be2306d2651ef98de65c249b256cafe768
korchasa <none@none>
2008-06-25 18:03:12 +0400

    [svn r7069] -- fix relative path
    --HG--
    branch : trunk

--- limb/fs/src/exception/lmbFileNotFoundException.class.php
+++ limb/fs/src/exception/lmbFileNotFoundException.class.php
@@ -2,11 +2,11 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
-lmb_require(dirname(__FILE__) . '/lmbFsException.class.php');
+lmb_require('limb/fs/src/exception/lmbFsException.class.php');
 
 /**
  * class lmbFileNotFoundException.
