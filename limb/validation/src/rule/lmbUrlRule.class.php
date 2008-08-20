2d2a80aefd2c81352697d7e4d68b70888be5342c
conf <none@none>
2008-08-20 14:41:55 +0400

    [svn r7150] -- lmbUrlRule is completely rewritten using regexps. -- fixed tests.
    --HG--
    branch : trunk

--- limb/validation/src/rule/lmbUrlRule.class.php
+++ limb/validation/src/rule/lmbUrlRule.class.php
@@ -6,52 +6,26 @@
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
  */
-lmb_require('limb/validation/src/rule/lmbDomainRule.class.php');
+
+lmb_require('limb/validation/src/rule/lmbDomainRule.class.php');  
 
 /**
- * Checks that field value is a valid Url.
+ * Checks that field value is a valid url.
  * @package validation
- * @version $Id: lmbUrlRule.class.php 6467 2007-10-29 15:24:16Z serega $
+ * @version $Id: lmbUrlRule.class.php 7150 2008-08-20 10:41:55Z conf $
  */
 class lmbUrlRule extends lmbDomainRule
 {
   /**
-  * @var array List of allowable schemes e.g. array('http', 'ftp');
+  * @var pattern to match an url
   */
-  protected $allowable_schemes =  array();
-
-  /**
-  * Constructor.
-  * @param string Field name
-  * @param array List of allowable schemes
-  */
-  function __construct($field_name, $allowable_schemes = array(), $custom_error = '')
-  {
-    parent :: __construct($field_name, $custom_error);
-
-    $this->allowable_schemes = $allowable_schemes ? $allowable_schemes : array();
-  }
-
+   protected $pattern = '#^((?<protocol>https?|ftp)://)?(?<domain>[-A-Z0-9.]+)(?<file>/[-A-Z0-9+&@\#/%=~_|!:,.;]*)?(?<parameters>\?[-A-Z0-9+&@\#/%=~_|!:,.;]*)?$#i';
+   
   function check($value)
   {
-    $url = @parse_url($value);
-    if (isset($url['scheme']) || isset($url['host']) &&
-        ($url['scheme'] == 'http' || $url['scheme'] == 'ftp'))
-    {
-      parent::check($url['host']);
-    }
+    if(!preg_match($this->pattern, $value, $matches))
+      return $this->error('{Field} is not an url.');
 
-    if (!sizeof($this->allowable_schemes))
-      return;
-
-    if (!isset($url['scheme']))
-    {
-      $this->error('Please specify a scheme for {Field}.');
-      return;
-    }
-
-    if (!in_array($url['scheme'], $this->allowable_schemes))
-      $this->error('{Field} may not use {scheme}.', array('scheme' => $url['scheme']));
+    parent::check($matches['domain']);
   }
 }
-
--- limb/validation/tests/cases/rule/lmbUrlRuleTest.class.php
+++ limb/validation/tests/cases/rule/lmbUrlRuleTest.class.php
@@ -18,7 +18,14 @@ class lmbUrlRuleTest extends lmbValidationRuleTestCase
     $dataspace->set('testfield', 'http://www.sourceforge.net/');
 
     $this->error_list->expectNever('addError');
-
+    $rule->validate($dataspace, $this->error_list);
+    
+    $dataspace->set('testfield', 'https://www.sourceforge.net/');
+    $this->error_list->expectNever('addError');
+    $rule->validate($dataspace, $this->error_list);
+        
+    $dataspace->set('testfield', 'ftp://www.sourceforge.net/');
+    $this->error_list->expectNever('addError');
     $rule->validate($dataspace, $this->error_list);
   }
   
@@ -34,38 +41,6 @@ class lmbUrlRuleTest extends lmbValidationRuleTestCase
     $rule->validate($dataspace, $this->error_list);
   }
   
-  function testUrlRuleBadScheme()
-  {
-    $allowedSchemes = array('http');
-    $rule = new lmbUrlRule('testfield',$allowedSchemes);
-
-    $dataspace = new lmbSet();
-    $dataspace->set('testfield', 'ftp://www.sourceforge.net/');
-
-    $this->error_list->expectOnce('addError',
-                                  array(lmb_i18n('{Field} may not use {scheme}.', 'validation'),
-                                        array('Field'=>'testfield'),
-                                        array('scheme'=>'ftp')));
-
-    $rule->validate($dataspace, $this->error_list);
-  }
-
-  function testUrlRuleMissingScheme()
-  {
-    $allowedSchemes = array('http');
-    $rule = new lmbUrlRule('testfield',$allowedSchemes);
-
-    $dataspace = new lmbSet();
-    $dataspace->set('testfield', 'www.sourceforge.net/');
-
-    $this->error_list->expectOnce('addError',
-                                  array(lmb_i18n('Please specify a scheme for {Field}.', 'validation'),
-                                        array('Field'=>'testfield'),
-                                        array()));
-
-    $rule->validate($dataspace, $this->error_list);
-  }
-
   function testUrlRuleDomain()
   {
     $rule = new lmbUrlRule('testfield');
@@ -73,46 +48,38 @@ class lmbUrlRuleTest extends lmbValidationRuleTestCase
     $dataspace = new lmbSet();
     $dataspace->set('testfield', 'http://www.source--forge.net/');
 
-    $this->error_list->expectOnce('addError',
-                                  array(lmb_i18n('{Field} may not contain double hyphens (--).', 'validation'),
-                                        array('Field'=>'testfield'),
-                                        array()));
+    $this->error_list->expectOnce(
+      'addError',
+      array(
+        lmb_i18n('{Field} may not contain double hyphens (--).', 'validation'),
+        array('Field'=>'testfield'),
+        array()
+      )
+    );
 
     $rule->validate($dataspace, $this->error_list);
   }
 
   function testUrlRuleDomainWithCustomError()
   {
-    $rule = new lmbUrlRule('testfield', '', 'Custom_Error');
+    $rule = new lmbUrlRule('testfield', 'Custom_Error');
 
     $dataspace = new lmbSet();
     $dataspace->set('testfield', 'http://www.source--forge.net/');
 
-    $this->error_list->expectOnce('addError',
-                                  array('Custom_Error',
-                                        array('Field'=>'testfield'),
-                                        array()));
-
-    $rule->validate($dataspace, $this->error_list);
-  }
-
-  function testUrlRuleBadSchemeWithCustomError()
-  {
-    $allowedSchemes = array('http');
-    $rule = new lmbUrlRule('testfield', $allowedSchemes, 'Custom_error');
-
-    $dataspace = new lmbSet();
-    $dataspace->set('testfield', 'ftp://www.sourceforge.net/');
-
-    $this->error_list->expectOnce('addError',
-                                  array('Custom_error',
-                                        array('Field'=>'testfield'),
-                                        array('scheme'=>'ftp')));
+    $this->error_list->expectOnce(
+      'addError',
+      array(
+        'Custom_Error',
+        array('Field'=>'testfield'),
+        array()
+      )
+    );
 
     $rule->validate($dataspace, $this->error_list);
   }
   
-  function testUrlRuleWithBadTextWithoutAllowableSchemes()
+  function testUrlRuleWithGarbage()
   {    
     $rule = new lmbUrlRule('testfield');
     
