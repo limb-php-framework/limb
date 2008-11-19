72896c013326b9535ff20bf04b1fc783520ab6e4
korchasa <none@none>
2008-11-19 04:28:22 +0300

    [svn r7232] -- added clip macro filter
    --HG--
    branch : trunk

---
+++ limb/macro/src/filters/clip.filter.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */ 
+ 
+/**
+ * class lmbMacroClipFilter.
+ * Does nothing. Used in case if you need to cancel default html filter but not need any other filters to be applied.
+ *
+ * @filter clip
+ * @package macro
+ * @version $Id$
+ */ 
+class lmbMacroClipFilter extends lmbMacroFilter
+{
+  function getValue()
+  {
+    return 'substr('.$this->base->getValue().', 0, '.$this->params[0].')';
+  }  
+} 
---
+++ limb/macro/tests/cases/filters/lmbMacroClipFilterTest.class.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+class lmbMacroClipFilterTest extends lmbBaseMacroTest
+{
+  function testStatic()
+  {
+    $code = '{$#str|clip:1}{$#str|clip:2}{$#str|clip:3}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+    $tpl->set('str', '12');
+    $out = $tpl->render();
+    $this->assertEqual($out, '11212');        
+  }  
+  
+  function testDinamic()
+  {
+    $code = '{$#str|clip:$#cnt}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+    $tpl->set('str', 'foo');
+    $tpl->set('cnt', '1');
+    $out = $tpl->render();
+    $this->assertEqual($out, 'f');        
+  }  
+  
+}
+
