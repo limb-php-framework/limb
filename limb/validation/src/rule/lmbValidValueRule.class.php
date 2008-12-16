c7b9d56e93d3fb0b69f9e458c3c08092e7bfc641
korchasa <none@none>
2008-12-16 18:59:17 +0300

    [svn r7366] -- added lmbValidValueRule
    --HG--
    branch : trunk

---
+++ limb/validation/src/rule/lmbValidValueRule.class.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com 
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ */
+lmb_require('limb/validation/src/rule/lmbSingleFieldRule.class.php');
+
+/**
+ * Checks that field value is equal some valid value
+ * Example of usage:
+ * <code>
+ * lmb_require('limb/validation/src/rule/lmbValidValueRule.class.php');
+ * $validator->addRule(new lmbValidValueRule("CAPTCHA", 'asdh21'));
+ * </code>
+ * @package validation
+ * @version $Id: lmbInvalidValueRule.class.php 6243 2007-08-29 11:53:10Z pachanga $
+ */
+class lmbValidValueRule extends lmbSingleFieldRule
+{
+  protected $valid_value;
+
+  function __construct($field_name, $valid_value, $custom_error = '')
+  {
+    parent :: __construct($field_name, $custom_error);
+
+    $this->valid_value = $valid_value;
+  }
+
+  function check($value)
+  {
+    if ($value != $this->valid_value)
+    {
+      $this->error('{Field} value is wrong');
+    }
+  }
+}
+
+
---
+++ limb/validation/tests/cases/rule/lmbValidValueRuleTest.class.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com 
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ */
+require('limb/validation/src/rule/lmbValidValueRule.class.php');
+
+class lmbValidValueRuleTest extends lmbValidationRuleTestCase
+{
+  function testValidValueRule_Success_Int()
+  {
+    $rule = new lmbValidValueRule('testfield', 1);
+
+    $data = new lmbSet();
+    $data->set('testfield', 1);
+
+    $this->error_list->expectNever('addError');
+
+    $rule->validate($data, $this->error_list);
+  }
+  
+  function testValidValueRule_Error_Int()
+  {
+    $rule = new lmbValidValueRule('testfield', 1);
+
+    $data = new lmbSet();
+    $data->set('testfield', 0);
+
+    $this->error_list->expectOnce('addError', array(
+      lmb_i18n('{Field} value is wrong', 'validation'),
+      array('Field' => 'testfield'),
+      array()
+    ));
+
+    $rule->validate($data, $this->error_list);
+  }
+
+  function testValidValueRule_Success_IntAndString()
+  {
+    $rule = new lmbValidValueRule('testfield', 1);
+
+    $data = new lmbSet();
+    $data->set('testfield', '1');
+
+    $this->error_list->expectNever('addError');
+
+    $rule->validate($data, $this->error_list);
+  }  
+
+  function testInvalidValueRule_Success_Bool()
+  {
+    $rule = new lmbValidValueRule('testfield', false);
+
+    $data = new lmbSet();
+    $data->set('testfield', 0);
+
+    $this->error_list->expectNever('addError');
+
+    $rule->validate($data, $this->error_list);
+
+  }  
+}
+
+
