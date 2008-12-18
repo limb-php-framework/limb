4d49d40badc58fe23d79dc1c93350dac569504eb
korchasa <none@none>
2008-12-18 06:05:47 +0300

    [svn r7396] -- phpdocs
    --HG--
    branch : trunk

--- limb/cli/src/lmbCliBaseCmd.class.php
+++ limb/cli/src/lmbCliBaseCmd.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 lmb_require('limb/cli/src/lmbCliInput.class.php');
 
@@ -16,6 +16,11 @@ lmb_require('limb/cli/src/lmbCliInput.class.php');
  */
 abstract class lmbCliBaseCmd
 {
+  /**
+   * @var lmbCliResponse
+   */
+  protected $output;
+
   function __construct($output)
   {
     $this->output = $output;
