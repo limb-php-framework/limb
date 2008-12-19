2750673b8e1d96316bfa73ea4d49c0ef1eb725b6
korchasa <none@none>
2008-12-19 16:03:54 +0300

    [svn r7429] -- configs fixed
    --HG--
    branch : trunk

--- limb/view/settings/wact.conf.php
+++ limb/view/settings/wact.conf.php
@@ -4,7 +4,7 @@ $conf = array(
   'forcescan' => false,  #Force to scan directories for tags, filters and properties (very slow)
   'forcecompile' => true, #Force every template to be re-compiled on every request. Option is used
                        #for debugging templates when developing template generation code
-  'tpl_scan_dirs' => 'template;limb/*/template',
-  'tags_dirs' => 'src/wact','limb/*/src/wact','limb/wact/src/tags','src/template/tags','limb/*/src/template/tags','limb/wact/src/tags'
+  'tpl_scan_dirs' => array('template','limb/*/template'),
+  'tags_dirs' => array('src/wact','limb/*/src/wact','limb/wact/src/tags','src/template/tags','limb/*/src/template/tags','limb/wact/src/tags')
 );
 
