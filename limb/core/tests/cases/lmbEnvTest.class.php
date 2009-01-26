124290092e47a241a537bb33bdb804744bdba337
korchasa <none@none>
2009-01-26 13:38:37 +0300

    [svn r7483] -- added lmbEnv::trace() for debug
    --HG--
    branch : trunk

--- limb/core/tests/cases/lmbEnvTest.class.php
+++ limb/core/tests/cases/lmbEnvTest.class.php
@@ -23,4 +23,29 @@ class lmbEnvTest extends UnitTestCase
     lmbEnv::set('foo', 'baz');
     $this->assertEqual(lmbEnv::get('foo'), 'baz');
   }
+  
+  function testTrace()
+  {    
+    lmbEnv::trace('foo');
+    
+    ob_start();
+    lmbEnv::add($key = 'foo', $value = 'bar');
+    $trace_info = ob_get_clean();
+    
+    $this->assertTrue(strstr($trace_info, __FILE__));
+    $this->assertTrue(strstr($trace_info, $call_line = '32')); 
+    $this->assertTrue(strstr($trace_info, $method_name = 'add')); 
+    $this->assertTrue(strstr($trace_info, $key));
+    $this->assertTrue(strstr($trace_info, $value));     
+        
+    ob_start();
+    lmbEnv::set($key, $value = 'baz');    
+    $trace_info = ob_get_clean();
+    
+    $this->assertTrue(strstr($trace_info, __FILE__));
+    $this->assertTrue(strstr($trace_info, $call_line = '42'));
+    $this->assertTrue(strstr($trace_info, $method_name = 'set')); 
+    $this->assertTrue(strstr($trace_info, $key));
+    $this->assertTrue(strstr($trace_info, $value));
+  }
 }
