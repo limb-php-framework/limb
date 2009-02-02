1df04eae0d8145f9c6f1a1a1fc641d6cf53d8b5e
pachanga <none@none>
2009-02-02 21:50:58 +0300

    [svn r7571] -- initial refactoring of tests
    --HG--
    branch : trunk

--- limb/taskman/tests/taskman.test.php
+++ limb/taskman/tests/taskman.test.php
@@ -4,15 +4,86 @@
 
 class TaskmanTest extends UnitTestCase 
 {  
-  function testAcceptance()
+  function testRunOneTask()
   {
-    $output = '';
-    $logfile = LIMB_VAR_DIR.'/taskman.log';
-    file_put_contents($logfile, '');
-    
-    $str = 'php '.dirname(__FILE__).'/../example/build.php "build|remove_old" s -b -D LOG='.$logfile;
-    exec($str, $output);
-    
-    $this->assertEqual('ssosursb', file_get_contents($logfile));
+    list($code, $out) = $this->_run("
+    function task_foo() { echo 'foo'; }
+    ",
+    '-b foo');
+
+    $this->assertEqual(0, $code);
+    $this->assertEqual("foo", $out);
+  }
+
+  function testRunSeveralTasksFromCLI()
+  {
+    list($code, $out) = $this->_run("
+    function task_foo() { echo 'foo'; }
+    function task_bar() { echo 'bar'; }
+    ",
+    '-b foo,bar');
+
+    $this->assertEqual(0, $code);
+    $this->assertEqual("foobar", $out);
+  }
+
+  function testRunDependedentTask()
+  {
+    list($code, $out) = $this->_run("
+    /**
+     * @deps bar
+     */
+    function task_foo() { echo 'foo'; }
+    function task_bar() { echo 'bar'; }
+    ",
+    '-b foo');
+
+    $this->assertEqual(0, $code);
+    $this->assertEqual("barfoo", $out);
+  }
+
+  function testRunInterDependedentTasks()
+  {
+    list($code, $out) = $this->_run("
+    /**
+     * @deps zoo
+     */
+    function task_foo() { echo 'foo'; }
+    function task_bar() { echo 'bar'; }
+    /**
+     * @deps bar,foo,wow
+     */
+    function task_zoo() { echo 'zoo'; }
+    function task_wow() { echo 'wow'; }
+    ",
+    '-b foo');
+
+    $this->assertEqual(0, $code);
+    $this->assertEqual("barwowzoofoo", $out);
+  }
+
+  function testParallTasks()
+  {
+    list($code, $out) = $this->_run("
+    function task_foo() { sleep(1);echo 'foo'; }
+    function task_bar() { sleep(2);echo 'bar'; }
+    /**
+     * @deps bar|foo|wow
+     */
+    function task_zoo() { echo 'zoo'; }
+    function task_wow() { sleep(3);echo 'wow'; }
+    ",
+    '-b zoo');
+
+    $this->assertEqual(0, $code);
+    //$this->assertEqual("", $out);
+  }
+
+  protected function _run($contents, $cmd)
+  {
+    $file = LIMB_VAR_DIR . '/taskman-script.' . mt_rand() . '.php';
+    file_put_contents($file, "<?php\nrequire_once('" . dirname(__FILE__) . "/../taskman.inc.php');\ntaskman_run();\n$contents");
+    exec("php $file $cmd", $out, $res);
+    return array($res, implode($out));
   }
 }
