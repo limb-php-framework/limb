ae4d4d7b3a9231df59e5136c26eeecd87102611a
conf <none@none>
2009-03-11 13:47:01 +0300

    [svn r7726] -- changed test table's name to lmb_cache2
    --HG--
    branch : trunk

--- limb/cache2/tests/cases/drivers/lmbCacheDbConnectionTest.class.php
+++ limb/cache2/tests/cases/drivers/lmbCacheDbConnectionTest.class.php
@@ -15,41 +15,41 @@ lmb_require('limb/dbal/src/lmbDbDump.class.php');
 class lmbCacheDbConnectionTest extends lmbCacheConnectionTest
 {
   protected $storage_init_file = 'limb/dbal/common.inc.php';
-  
+
   function _getDumpFile()
-  {  	
-  	$type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
-    
+  {
+    $type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
+
     $dump_file = dirname(__FILE__) . '/../../../init/cache.' . $type;
     return file_exists($dump_file) ? $dump_file : null;
   }
 
   function skip()
-  {  	
-  	$is_connection_exists = lmbToolkit :: instance()->isDefaultDbDSNAvailable();
-  	$this->skipIf(!$is_connection_exists, 'DB connection not found. Test skiped');
-  	if($is_connection_exists)
+  {
+    $is_connection_exists = lmbToolkit :: instance()->isDefaultDbDSNAvailable();
+    $this->skipIf(!$is_connection_exists, 'DB connection not found. Test skiped');
+    if($is_connection_exists)
       $this->skipIf(!$this->_getDumpFile(), 'Dump file for type "'.lmbToolkit :: instance()->getDefaultDbConnection()->getType().'" not found. Test skiped');
-  }  
-  
+  }
+
   function setUp()
   {
     $this->dump = new lmbDbDump($this->_getDumpFile());
-    $this->dump->load();    	
-    
+    $this->dump->load();
+
     parent::setUp();
   }
-  
+
   function tearDown()
   {
     parent::tearDown();
-    
+
     $this->dump->clean();
   }
-   
+
   function __construct()
   {
-    $this->dsn = 'db://dsn?table=lmb_cache';
+    $this->dsn = 'db://dsn?table=lmb_cache2';
     parent::__construct();
   }
 }
