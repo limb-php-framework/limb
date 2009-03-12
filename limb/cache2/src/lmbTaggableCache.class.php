cd374a39fafecc9cc94b6312246deb5957e12091
conf <none@none>
2009-03-12 09:47:01 +0300

    [svn r7733] -- added array type conversion in lmbTaggableCache due to bugs in memcached engine -- uncommented assert in lmbTaggableCacheTest
    --HG--
    branch : trunk

--- limb/cache2/src/lmbTaggableCache.class.php
+++ limb/cache2/src/lmbTaggableCache.class.php
@@ -8,7 +8,7 @@ class lmbTaggableCache implements lmbCache
    */
   protected $connection;
   public $tags_prefix = 'tag42_';
-  
+
   function __construct(lmbCacheConnection $connection)
   {
     $this->connection = $connection;
@@ -27,79 +27,79 @@ class lmbTaggableCache implements lmbCache
 
     return $new_keys;
   }
-  
+
   protected function _createContainer($value, $tags)
   {
     $tags = $this->_resolveTagsKeys($tags);
-    $tags_values = $this->connection->get($tags);
-    
+    $tags_values = (array) $this->connection->get($tags);
+
     foreach($tags_values as $tag_key => $tag_value)
       if(is_null($tag_value))
       {
         $tags_values[$tag_key] = 0;
         $this->connection->add($tag_key, 0);
       }
-    
+
     return array('tags' => $tags_values, 'value' => $value);
   }
-  
+
   protected function _isTagsValid($tags)
   {
-    $tags_versions = $this->connection->get(array_keys($tags));
-    
+    $tags_versions = (array) $this->connection->get(array_keys($tags));
+
     foreach($tags_versions as $tag_key => $tag_version)
       if(is_null($tag_version) || $tags[$tag_key] !== $tag_version)
         return false;
-    
+
     return true;
   }
-  
+
   protected function _getFromContainer($container)
-  {        
+  {
     if($this->_isTagsValid($container['tags']))
       return $container['value'];
     else
       return NULL;
   }
-    
+
   function add($key, $value, $ttl = false, $tags_keys = false)
   {
     if(!is_array($tags_keys))
       $tags_keys = array($tags_keys);
-    
+
     return $this->connection->add($key, $this->_createContainer($value, $tags_keys), $ttl);
   }
-  
+
   function set($key, $value, $ttl = false, $tags_keys = false)
   {
     if(!is_array($tags_keys))
       $tags_keys = array($tags_keys);
-    
+
     return $this->connection->set($key, $this->_createContainer($value, $tags_keys), $ttl);
   }
-  
+
   function get($key)
   {
     if(is_null($container = $this->connection->get($key)))
       return NULL;
-      
+
     if(is_null($value = $this->_getFromContainer($container)))
       $this->connection->delete($key);
-      
+
     return $value;
   }
-  
+
   function delete($key)
   {
     $this->connection->delete($key);
   }
-  
+
   function deleteByTag($tag)
   {
     $tag = $this->_resolveTagsKeys($tag);
     $this->connection->increment($tag);
   }
-  
+
   function flush()
   {
     $this->connection->flush();
--- limb/cache2/tests/cases/lmbTaggableCacheTest.class.php
+++ limb/cache2/tests/cases/lmbTaggableCacheTest.class.php
@@ -9,54 +9,54 @@ class lmbTaggableCacheTest extends UnitTestCase
    * @var lmbTaggableCache
    */
   protected $cache;
-  
+
   function setUp()
   {
     $this->cache = new lmbTaggableCache(lmbCacheFactory::createConnection('file:///'.lmb_env_get('LIMB_VAR_DIR').'/cache'));
   }
-  
+
   function tearDown()
   {
     $this->cache->flush();
   }
-  
+
   protected function _createId()
   {
     return 'id_'.microtime();
   }
-  
+
   function testAdd()
   {
     $this->assertTrue($this->cache->add($key = $this->_createId(), $value = 'value', null, 'tag'));
-    //$this->assertFalse($this->cache->add($key, 'another_value', null, 'tag'));
-    
+    $this->assertFalse($this->cache->add($key, 'another_value', null, 'tag'));
+
     $this->assertEqual($this->cache->get($key), $value);
   }
-  
+
   function testDeleteByTags_SingleTag()
   {
     $this->cache->set($key = $this->_createId(), $value = 'value', null, 'tag_delete');
-    
+
     $this->cache->deleteByTag('tag_delete');
-    
+
     $this->assertNull($this->cache->get($key));
   }
-  
+
   function testDeleteByTags_MultipleTag()
   {
     $this->cache->set($key = $this->_createId(), $value = 'value', null, array('tag1','tag2'));
-    
+
     $this->cache->deleteByTag('tag1');
-    
+
     $this->assertNull($this->cache->get($key));
   }
-  
+
   function testDeleteByTags_DifferentTag()
   {
     $this->cache->set($key = $this->_createId(), $value = 'value', null, 'tag');
-    
+
     $this->cache->deleteByTag('different_tag');
-    
+
     $this->assertIdentical($this->cache->get($key), $value);
   }
 
