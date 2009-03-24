ffb7faad254034ce6d734a4cc8af828dfc1f213c
conf <none@none>
2009-03-24 17:05:00 +0300

    [svn r7812] -- added skipif for crashed wact tests in i18n package -- fixed some typos
    --HG--
    branch : trunk

---
+++ limb/i18n/tests/cases/wact/.skipif.php
@@ -0,0 +1,5 @@
+<?php
+$test_dir = basename(dirname(__FILE__));
+echo "i18n package '$test_dir' tests are skipped! (no wact support anymore)\n\n";
+
+return true;
--- limb/web_spider/src/lmbSearchIndexingObserver.class.php
+++ limb/web_spider/src/lmbSearchIndexingObserver.class.php
@@ -2,16 +2,16 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 
 /**
  * class lmbSearchIndexingObserver.
  *
  * @package web_spider
- * @version $Id: lmbSearchIndexingObserver.class.php 7686 2009-03-04 19:57:12Z korchasa $
+ * @version $Id: lmbSearchIndexingObserver.class.php 7812 2009-03-24 14:05:00Z conf $
  */
 class lmbSearchIndexingObserver
 {
@@ -29,11 +29,11 @@ class lmbSearchIndexingObserver
 
     $this->counter++;
 
-    echo "\n{$this->counter})started indexing " . $uri->toString() . "...";
+    echo "{$this->counter})started indexing " . $uri->toString() . "...";
 
     $this->indexer->index($uri, $reader->getContent());
 
-    echo "done";
+    echo "done\n";
   }
 }
 
