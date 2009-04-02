aedbccc834c23d9c3f4eca9c5b7ad9343c7ce96e
slevin <none@none>
2009-04-02 15:18:27 +0400

    [svn r7835] -- fixed for undefined function lmb_env_setor() on setup file
    --HG--
    branch : trunk

--- limb/web_app/skel/setup.php
+++ limb/web_app/skel/setup.php
@@ -9,7 +9,7 @@
 
 /**
  * @package web_app
- * @version $Id: setup.php 7676 2009-03-03 22:37:28Z korchasa $
+ * @version $Id: setup.php 7835 2009-04-02 11:18:27Z slevin $
  */
 set_include_path(dirname(__FILE__) . PATH_SEPARATOR .
                  dirname(__FILE__) . '/lib/' . PATH_SEPARATOR);
@@ -17,8 +17,7 @@ set_include_path(dirname(__FILE__) . PATH_SEPARATOR .
 if(file_exists(dirname(__FILE__) . '/setup.override.php'))
   require_once(dirname(__FILE__) . '/setup.override.php');
 
-lmb_env_setor('LIMB_VAR_DIR', dirname(__FILE__) . '/var/');
-
 require_once('limb/core/common.inc.php');
 require_once('limb/web_app/common.inc.php');
 
+lmb_env_setor('LIMB_VAR_DIR', dirname(__FILE__) . '/var/');
