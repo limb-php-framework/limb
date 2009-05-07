bfdd57e21d0a50141a0f3b7ff97b17e5bbc25fef
alex433 <none@none>
2009-05-07 16:11:29 +0400

    [svn r7918] -- upgrading tinyMCE to version 3.2.3.1 -- updating configuration settings -- adding editor interface multi languages support -- adding spellchecker plugin
    --HG--
    branch : trunk

--- limb/wysiwyg/settings/wysiwyg.conf.php
+++ limb/wysiwyg/settings/wysiwyg.conf.php
@@ -22,30 +22,38 @@ $conf = array(
     'rows' => 15,
     'base_path' => '/shared/wysiwyg/tiny_mce/',
     'editor' => array(
+      'language' => 'en',
       'mode' => "textareas",
       'theme' => "advanced",
-      'plugins' => "spellchecker,style,layer,table,save,advhr,advimage,ibrowser,advlink,emotions,iespell,insertdatetime,preview,zoom,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
-      'theme_advanced_buttons1_add_before' => "save,newdocument,separator",
-      'theme_advanced_buttons1_add' => "fontselect,fontsizeselect",
-      'theme_advanced_buttons2_add' => "separator,insertdate,inserttime,preview,separator,forecolor,backcolor",
-      'theme_advanced_buttons2_add_before' => "cut,copy,paste,pastetext,pasteword,separator,search,replace,separator",
-      'theme_advanced_buttons3_add_before' => "tablecontrols,separator",
-      'theme_advanced_buttons3_add' => "ibrowser, emotions,iespell,media,advhr,separator,print,separator,ltr,rtl,separator,fullscreen",
-      'theme_advanced_buttons4' => "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,spellchecker,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template",
+
+      'plugins' => "safari,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+      'theme_advanced_buttons1' => "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+      'theme_advanced_buttons2' => "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+      'theme_advanced_buttons3' => "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+      'theme_advanced_buttons4' => "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
+
       'theme_advanced_toolbar_location' => "top",
       'theme_advanced_toolbar_align' => "left",
       'theme_advanced_path_location' => "bottom",
+
       'plugin_insertdate_dateFormat' => "%Y-%m-%d",
       'plugin_insertdate_timeFormat' => "%H:%M:%S",
-      'extended_valid_elements' => "hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style],p[lang]",
-      'external_link_list_url' => "example_data/example_link_list.js",
-      'external_image_list_url' => "example_data/example_image_list.js",
-      'flash_external_list_url' => "example_data/example_flash_list.js",
-      'template_external_list_url' => "example_data/example_template_list.js",
+
       'theme_advanced_resize_horizontal' => false,
       'theme_advanced_resizing' => true,
       'apply_source_formatting' => true,
-      'spellchecker_languages' => "+English=>en,Danish=>da,Dutch=>nl,Finnish=>fi,French=>fr,German=>de,Italian=>it,Polish=>pl,Portuguese=>pt,Spanish=>es,Swedish=>sv",
+
+      'spellchecker_languages' => "+English=>en,Danish=>da,Dutch=>nl,Finnish=>fi,French=>fr,German=>de,Italian=>it,Polish=>pl,Portuguese=>pt,Russian=>ru,Spanish=>es,Swedish=>sv,Ukrainian=>uk",
+
+      // Example content CSS (should be your site CSS)
+      'content_css' > "css/example.css",
+
+      // Drop lists for link/image/media/template dialogs
+      'template_external_list_url' => "js/template_list.js",
+      'external_link_list_url' => "js/link_list.js",
+      'external_image_list_url' => "js/image_list.js",
+      'media_external_list_url' => "js/media_list.js",
     )
   )
 );
