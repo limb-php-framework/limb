78c0272cedd47143eff0bb392353772e63034b71
pachanga <none@none>
2009-06-26 08:30:18 +0400

    [svn r7967] -- adding test for mysql extension
    --HG--
    branch : trunk

---
+++ limb/dbal/tests/cases/driver/mysql/lmbMysqlExtensionTest.class.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com 
+ * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ */
+
+require_once(dirname(__FILE__) . '/fixture.inc.php');
+
+class lmbMysqlExtensionTest extends UnitTestCase
+{
+  function setUp()
+  {
+    $this->connection = lmbToolkit :: instance()->getDefaultDbConnection();
+    DriverMysqlSetup($this->connection->getConnectionId());
+  }
+
+  function testConcat()
+  {
+    $stmt = $this->connection->newStatement("SELECT " . $this->connection->getExtension()->concat(array('"1"', '"2"', '"foo"')) . " AS c ");
+    $record = $stmt->getOneRecord();
+    $this->assertEqual($record->get('c'), "12foo");
+  }
+
+  function testSubstring()
+  {
+    $stmt = $this->connection->newStatement("SELECT " . $this->connection->getExtension()->substr('"fco"', 2, 1) . " AS c ");
+    $record = $stmt->getOneRecord();
+    $this->assertEqual($record->get('c'), "c");
+  }
+
+  function testSubstringWithoutLimit()
+  {
+    $stmt = $this->connection->newStatement("SELECT " . $this->connection->getExtension()->substr('"fco"', 2) . " AS c ");
+    $record = $stmt->getOneRecord();
+    $this->assertEqual($record->get('c'), "co");
+  }
+}
+
+
