b7b54727e760ffdbf40bda7a68c91b7f66c0d68d
idler <none@none>
2009-08-08 22:29:00 +0400

    [svn r7978] -- added i18n tag for macro
    --HG--
    branch : trunk

---
+++ limb/i18n/src/macro/i18n.tag.php
@@ -0,0 +1,33 @@
+<?php
+ /*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+
+/**
+ * @tag i18n
+ * @aliases __
+ * @req_attributes text
+ * @forbid_end_tag
+ */
+class i18nMacroTag extends lmbMacroTag
+{
+
+  function preParse($compiller)
+  {
+     if(!$this->has('text')) throw new lmbMacroException('tag {{i18n}} required attribute "text"');
+     parent :: preParse($compiller);
+  }
+
+  protected function _generateContent($code)
+  {
+    $code->writePHP('
+    echo lmb_i18n(\''.$this->get('text').'\',\''.($this->has('domain')?$this->get('domain'):'default').'\');
+        ');
+  }
+
+}
