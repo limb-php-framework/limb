b1f835d96c2484efa956af8ea6c6cc8a1af33661
3dmax <none@none>
2009-08-24 19:53:36 +0400

    [svn r7985] -- skip rewriting file locator cache when searched file not found
    --HG--
    branch : trunk

--- limb/fs/src/lmbCachingFileLocator.class.php
+++ limb/fs/src/lmbCachingFileLocator.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 lmb_require('limb/fs/src/lmbFileLocatorDecorator.class.php');
 lmb_require('limb/fs/src/lmbFs.class.php');
@@ -80,8 +80,8 @@ class lmbCachingFileLocator extends lmbFileLocatorDecorator
     if(isset($this->_cached_paths[$hash]))
       return $this->_cached_paths[$hash];
 
-    $this->_changed = true;
     $this->_cached_paths[$hash] = $this->locator->locate($alias, $params);
+    $this->_changed = true;
 
     return $this->_cached_paths[$hash];
   }
