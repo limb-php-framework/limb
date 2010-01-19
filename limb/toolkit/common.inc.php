6d920111b6e7a9373987d34dddc61b98b2d331a4
korchasa <none@none>
2010-01-19 23:11:45 +0300

    [svn r8034] -- use lmb_package_* in toolkit
    --HG--
    branch : trunk

---
+++ limb/toolkit/common.inc.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+/**
+ * @package dbal
+ * @version $Id: common.inc.php 7486 2009-01-26 19:13:20Z pachanga $
+ */
+require_once('limb/core/common.inc.php');
+
+lmb_package_register('toolkit', dirname(__FILE__));
--- limb/toolkit/tests/cases/.setup.php
+++ limb/toolkit/tests/cases/.setup.php
@@ -1,3 +1,2 @@
 <?php
 require_once('limb/core/common.inc.php');
-
