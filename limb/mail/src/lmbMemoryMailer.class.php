420ad644494f9c3289577ec617817a3bd2870b56
korchasa <none@none>
2010-01-20 00:55:59 +0300

    [svn r8046] -- added support for sending multiple messages
    --HG--
    branch : trunk

--- limb/mail/src/lmbMemoryMailer.class.php
+++ limb/mail/src/lmbMemoryMailer.class.php
@@ -10,17 +10,21 @@ class lmbMemoryMailer implements lmbBaseMailerInterface
   var $html;
   var $text;
   var $charset;
-  
+
+  static $mail_contents = array();
+
   function sendHtmlMail($recipients, $sender, $subject, $html, $text = null, $charset = 'utf-8')
-  {    
+  {
     $this->recipient = $recipients;
     $this->sender = $sender;
     $this->subject = $subject;
     $this->html = $html;
     $this->text = $text;
     $this->charset = $charset;
+
+    self::$mail_contents[] = $this->html;
   }
-  
+
   function sendPlainMail($recipients, $sender, $subject, $body, $charset = 'utf-8')
   {
     $this->recipient = $recipients;
@@ -28,5 +32,15 @@ class lmbMemoryMailer implements lmbBaseMailerInterface
     $this->subject = $subject;
     $this->text = $body;
     $this->charset = $charset;
-  }  
+  }
+
+  static function getMailContents()
+  {
+    return self::$mail_contents;
+  }
+
+  static function clearMailContents()
+  {
+    self::$mail_contents = array();
+  }
 }
