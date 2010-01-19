31fea8aaaa6a428656e4611f0bdf78c823930cef
korchasa <none@none>
2010-01-20 00:45:51 +0300

    [svn r8045] -- fixed: include view package for lmbMailService
    --HG--
    branch : trunk

--- limb/mail/src/lmbMailService.class.php
+++ limb/mail/src/lmbMailService.class.php
@@ -1,4 +1,5 @@
 <?php
+lmb_package_require('view');
 lmb_require('limb/core/src/lmbSet.class.php');
 lmb_require('limb/mail/src/lmbMailer.class.php');
 lmb_require('limb/mail/src/lmbMemoryMailer.class.php');
@@ -6,105 +7,105 @@ lmb_require('limb/mail/src/lmbResponseMailer.class.php');
 lmb_require('limb/macro/src/lmbMacroTemplate.class.php');
 
 class lmbMailService
-{  
+{
   protected $template_id;
   /**
    * @var Member
    */
   protected $member;
-  
+
   protected $dto;
   /**
    * @var lmbBaseMailerInterface
    */
   protected $mailer;
   protected $separator;
-  
+
   protected $subject;
   protected $html_content;
   protected $text_content;
 
   function __construct($template_id)
-  {        
-    $this->template_id = $template_id; 
+  {
+    $this->template_id = $template_id;
     $this->dto = new lmbSet;
     $this->separator = "\n\n";
-  }    
-  
+  }
+
   function set($name, $value)
   {
     $this->dto->set($name, $value);
   }
-  
+
   protected function _parseMailTemplate($postfix = '')
   {
   	$tools = lmbToolkit::instance();
-  	
+
     $template_file = $this->template_id . $postfix . '.phtml';
   	$path = $tools->locateTemplateByAlias('_mail/'.$template_file);
-  	
+
     $template = new lmbMacroTemplate($path, $tools->getConf('macro'));
-    $template->setVars($this->dto->export());    
+    $template->setVars($this->dto->export());
     $raw_content = $template->render();
-    
+
     $parts = explode($this->separator, $raw_content);
-    
+
     if(1 === count($parts))
       throw new lmbException('Subject must be on the top of mail template separated by "'.$this->separator.'"');
 
-    $this->subject = $parts[0];    
-    $this->text_content = $parts[1];    
-    
+    $this->subject = $parts[0];
+    $this->text_content = $parts[1];
+
     if(3 === count($parts))
       $this->html_content = $parts[2];
     else
       $this->html_content = $this->text_content;
   }
-  
+
   function getSubject()
-  {    
+  {
     if(is_null($this->subject))
      $this->_parseMailTemplate();
-     
+
     return $this->subject;
   }
-  
+
   function getHtmlContent()
-  {    
+  {
   	if(is_null($this->subject))
   	 $this->_parseMailTemplate();
-  	 
+
     return $this->html_content;
   }
-  
+
   function getTextContent()
   {
   	if(is_null($this->subject))
      $this->_parseMailTemplate();
-     
+
     return $this->text_content;
-  }  
-    
+  }
+
   function sendMailTo($email)
   {
-    $conf = lmbToolkit::instance()->getConf('mail');    
-    
-    $mailer_class = 'lmbMailer';    
-    
+    $conf = lmbToolkit::instance()->getConf('mail');
+
+    $mailer_class = 'lmbMailer';
+
     if(isset($conf['mode']))
       if($conf['mode'] == 'testing')
         $mailer_class = 'lmbMemoryMailer';
       elseif($conf['mode'] == 'devel')
         $mailer_class = 'lmbResponseMailer';
-    
-    $this->mailer = new $mailer_class($conf);    
+
+    $this->mailer = new $mailer_class($conf);
     $this->mailer->sendHtmlMail($email,
                           $conf['sender'],
                           $this->getSubject(),
                           $this->getHtmlContent(),
-                          $this->getTextContent());   
+                          $this->getTextContent());
   }
-  
+
   function getMailer()
   {
     return $this->mailer;
--- limb/mail/tests/cases/lmbMailServiceTest.class.php
+++ limb/mail/tests/cases/lmbMailServiceTest.class.php
@@ -1,7 +1,6 @@
 <?php
-
 lmb_require('limb/macro/tests/cases/lmbBaseMacroTest.class.php');
-lmb_require('limb/mail/src/lmbMailService.class.php');
+require_once('limb/mail/src/lmbMailService.class.php');
 
 class lmbMailServiceTest extends lmbBaseMacroTest
 {
@@ -11,44 +10,44 @@ class lmbMailServiceTest extends lmbBaseMacroTest
     lmbFs::mkdir($this->tpl_dir . '/_mail');
     lmbToolkit::instance()->setConf('macro', $this->_createMacroConfig());
   }
-  
-  function tearDown(){}  
-   
+
+  function tearDown(){}
+
   function testGetMailHtmlContent()
-  {          
+  {
   	$mail_template = <<<EOD
 subj
 
 {\$#foo}bar
 EOD;
-  	
-    $this->_createTemplate($mail_template, '_mail/baz.phtml');    
+
+    $this->_createTemplate($mail_template, '_mail/baz.phtml');
     $service = new lmbMailService('baz');
     $service->set('foo', 42);
-    
+
     $this->assertEqual('subj', $service->getSubject());
     $this->assertEqual('42bar', $service->getHtmlContent());
   }
-  
+
   function testGetMailTextContent()
-  {          
+  {
     $mail_template = <<<EOD
 subj
 
 {\$#bar}foo
 EOD;
-    
+
     $this->_createTemplate($mail_template, '_mail/baz.phtml');
-    
+
     $service = new lmbMailService('baz');
     $service->set('bar', 11);
-    
+
     $this->assertEqual('subj', $service->getSubject());
     $this->assertEqual('11foo', $service->getTextContent());
   }
-  
+
   function testGetMailBothContents()
-  {          
+  {
   	$mail_template = <<<EOD
 subj
 
@@ -56,13 +55,13 @@ subj
 
 {\$#html_dynamic}html_static
 EOD;
-    
+
     $this->_createTemplate($mail_template, '_mail/baz.phtml');
-    
+
     $service = new lmbMailService('baz');
     $service->set('text_dynamic', 42);
     $service->set('html_dynamic', 11);
-    
+
     $this->assertEqual('subj', $service->getSubject());
     $this->assertEqual('42text_static', $service->getTextContent());
     $this->assertEqual('11html_static', $service->getHtmlContent());
