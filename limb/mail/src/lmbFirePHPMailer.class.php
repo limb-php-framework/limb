286739f03f2556d0479301d831902c9a06858a50
korchasa <none@none>
2010-01-20 00:00:29 +0300

    [svn r8043] -- added FirePHP mailer
    --HG--
    branch : trunk

---
+++ limb/mail/src/lmbFirePHPMailer.class.php
@@ -0,0 +1,38 @@
+<?php
+
+lmb_require('limb/mail/src/lmbBaseMailerInterface.interface.php');
+
+class lmbFirePHPMailer implements lmbBaseMailerInterface
+{
+  function sendHtmlMail($recipients, $sender, $subject, $html, $text = null, $charset = 'utf-8')
+  {
+    $mail = array(
+      'recipients' => $recipients,
+      'sender' => $sender,
+      'subject' => $subject,
+      'html' => $html,
+      'text' => $text,
+      'charset' => $charset,
+    );
+    $this->_send($mail);
+  }
+
+  function sendPlainMail($recipients, $sender, $subject, $body, $charset = 'utf-8')
+  {
+    $mail = array(
+      'recipients' => $recipients,
+      'sender' => $sender,
+      'subject' => $subject,
+      'subject' => $subject,
+      'charset' => $charset,
+    );
+    $this->_send($mail);
+  }
+
+  protected function _send($content)
+  {
+    if(!function_exists('fb'))
+      throw new lmbException("FirePHP function 'fb' not found");
+    fb($content, 'new mail');
+  }
+}
