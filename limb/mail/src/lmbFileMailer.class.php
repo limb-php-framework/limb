429d20d4852ea03acc8432bdac917dd4b2815f0c
korchasa <none@none>
2010-01-20 00:57:20 +0300

    [svn r8047] -- added support for sending multiple messages
    --HG--
    branch : trunk

--- limb/mail/src/lmbFileMailer.class.php
+++ limb/mail/src/lmbFileMailer.class.php
@@ -6,14 +6,22 @@ class lmbFileMailer implements lmbBaseMailerInterface
 {
   function sendHtmlMail($recipients, $sender, $subject, $html, $text = null, $charset = 'utf-8')
   {
-    $content = '';
+    $content =<<<EOD
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head>
+<body>
+EOD;
     $content .= '<p>recipient: '.$recipients.'</p>';
     $content .= '<p>sender: '.$sender.'</p>';
     $content .= '<p>subject: '.$subject.'</p>';
     $content .= '<pre>html: '.$html.'</pre>';
     $content .= '<pre>text: '.$text.'</pre>';
     $content .= '<p>charset: '.$charset.'</p>';
-    lmbFs::safeWrite(lmb_env_get('LIMB_VAR_DIR').'/mail.log.html', $content);
+
+    $content .= '</body></html>';
+
+    $this->_send($recipients,$content);
   }
 
   function sendPlainMail($recipients, $sender, $subject, $body, $charset = 'utf-8')
@@ -24,6 +32,12 @@ class lmbFileMailer implements lmbBaseMailerInterface
     $content .= '<p>subject: '.$subject.'</p>';
     $content .= '<pre>text: '.$body.'</pre>';
     $content .= '<p>charset: '.$charset.'</p>';
-    lmbFs::safeWrite(lmb_env_get('LIMB_VAR_DIR').'/mail.log.txt', $content);
+
+    $this->_send($recipients,$content);
+  }
+
+  protected function _send($recipients, $content)
+  {
+    lmbFs::safeWrite(lmb_env_get('LIMB_VAR_DIR').'/mail/'.(pow(2,31) - time()).'_'.$recipients.'_'.microtime(true).'.txt', $content);
   }
 }
