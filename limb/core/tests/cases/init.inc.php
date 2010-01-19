f5ae2749912d895e634acbfa174e5d63f24bdccc
korchasa <none@none>
2010-01-19 03:04:47 +0300

    [svn r8031] -- include() replaced by include_once(), until the code contains require/include
    --HG--
    branch : trunk

--- limb/core/tests/cases/init.inc.php
+++ limb/core/tests/cases/init.inc.php
@@ -4,9 +4,9 @@ function lmb_tests_init_var_dir($value)
 {
   if(!is_dir($value) && !mkdir($value))
     throw new Exception("Could not create LIMB_VAR_DIR at '" . $value . "' during tests execution");
-  
-  lmb_env_setor('LIMB_VAR_DIR', realpath($value));  
-  
+
+  lmb_env_setor('LIMB_VAR_DIR', realpath($value));
+
   lmb_require('limb/fs/src/lmbFs.class.php');
   try { lmbFs :: mkdir(LIMB_VAR_DIR); } catch(lmbFsException $e) {}
 }
