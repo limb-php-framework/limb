1a4f8764c580f4870969027423fb79fa0c2e2376
korchasa <none@none>
2010-01-19 23:13:37 +0300

    [svn r8037] -- use lmb_package_* in calendar
    --HG--
    branch : trunk

---
+++ limb/calendar/common.inc.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+/**
+ * @package calendar
+ * @version $Id$
+ */
+require_once('limb/core/common.inc.php');
+lmb_package_register('calendar', dirname(__FILE__));
