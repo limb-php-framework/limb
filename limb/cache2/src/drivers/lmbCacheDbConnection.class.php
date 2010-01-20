3c08b33ac84934222e29b32da6dd494eb4d7e250
korchasa <none@none>
2010-01-20 06:57:28 +0300

    [svn r8064] -- fix: proper escaping SQL-values
    --HG--
    branch : trunk

--- limb/cache2/src/drivers/lmbCacheDbConnection.class.php
+++ limb/cache2/src/drivers/lmbCacheDbConnection.class.php
@@ -1,5 +1,5 @@
 <?php
-
+lmb_package_require('dbal');
 lmb_require('limb/cache2/src/drivers/lmbCacheAbstractConnection.class.php');
 lmb_require('limb/core/src/lmbSerializable.class.php');
 
@@ -13,38 +13,38 @@ class lmbCacheDbConnection extends lmbCacheAbstractConnection
    * @var lmbTableGateway
    */
   protected $db_table;
-  
+
   function __construct(lmbUri $dsn)
   {
     parent::__construct($dsn);
-    
+
     $this->dsn = $dsn;
-    
+
     if(!$db_dsn_name = $dsn->getHost())
       throw new lmbNoSuchPropertyException('DB DSN name not found in cache DSN', array('cache dsn'=>$dsn));
-      
+
     if(!$db_table_name = $dsn->getQueryItem('table'))
       throw new lmbNoSuchPropertyException('DB table name not found in cache DSN', array('cache dsn'=>$dsn));
-      
+
     $db_connection = lmbToolkit::instance()->getDbConnectionByName($db_dsn_name);
-    
+
     $this->db_table = new lmbTableGateway($db_table_name, $db_connection);
   }
-  
+
   function getType()
   {
     return 'Db';
   }
-  
+
   function lock($key)
   {
     $resolved_key = $this->_resolveKey($key);
-    
+
     $lock = $this->db_table->selectFirstRecord('`key` = \''.$resolved_key.'\'');
-    
+
     if($lock['is_locked'])
       return false;
-    
+
     try {
       $this->db_table->insertOnDuplicateUpdate(array(
         'key' => $resolved_key,
@@ -57,21 +57,21 @@ class lmbCacheDbConnection extends lmbCacheAbstractConnection
       return false;
     }
   }
-  
+
   function unlock($key)
   {
     $resolved_key = $this->_resolveKey($key);
-    
-    $this->db_table->update(array('is_locked' => false), '`key` = '.$resolved_key);
+
+    $this->db_table->update(array('is_locked' => false), '`key` = \''.$resolved_key.'\'');
   }
-  
+
   function add ($key, $value, $ttl = false)
   {
     $resolved_key = $this->_resolveKey($key);
-    
+
     if($ttl)
       $ttl += time();
-      
+
     try {
       $this->db_table->insert(array(
         'key' => $resolved_key,
@@ -85,14 +85,14 @@ class lmbCacheDbConnection extends lmbCacheAbstractConnection
       return false;
     }
   }
-  
+
   function set($key, $value, $ttl = false)
   {
     $resolved_key = $this->_resolveKey($key);
-    
+
     if($ttl)
       $ttl += time();
-      
+
     try {
       $this->db_table->insertOnDuplicateUpdate(array(
         'key' => $resolved_key,
@@ -106,28 +106,28 @@ class lmbCacheDbConnection extends lmbCacheAbstractConnection
       return false;
     }
   }
-  
+
   function _getSingleKeyValue ($key)
   {
     $resolved_key = $this->_resolveKey($key);
-    
+
     if(!$record = $this->db_table->selectFirstRecord('`key` = \''.$resolved_key.'\''))
       return NULL;
-      
+
     $ttl = (int) $record['ttl'];
-      
+
     if($ttl && $ttl < time())
       return NULL;
-      
+
     return lmbSerializable::unserialize($record['value']);
   }
-  
+
   function delete($key)
   {
     $resolved_key = $this->_resolveKey($key);
     $this->db_table->delete('`key` = \''.$resolved_key.'\'');
   }
-  
+
   function flush()
   {
     $this->db_table->delete();
