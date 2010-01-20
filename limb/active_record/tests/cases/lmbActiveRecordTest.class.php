e14a1c358e2e1f30a0aba9ea02dd6d71802140e3
korchasa <none@none>
2010-01-20 06:44:37 +0300

    [svn r8059] -- fixed: added require
    --HG--
    branch : trunk

--- limb/active_record/tests/cases/lmbActiveRecordTest.class.php
+++ limb/active_record/tests/cases/lmbActiveRecordTest.class.php
@@ -6,6 +6,7 @@
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
+lmb_require('limb/dbal/src/criteria/lmbSQLRawCriteria.class.php');
 
 class TestOneTableObjectWithCustomDestroy extends lmbActiveRecord
 {
