f6b703db09a3a62e93cf4a383fd413035a355f26
korchasa <none@none>
2010-01-22 03:55:01 +0300

    [svn r8081] -- fixed db setup/teardown for tests
    --HG--
    branch : trunk

--- limb/dbal/tests/cases/init.inc.php
+++ limb/dbal/tests/cases/init.inc.php
@@ -1,19 +1,29 @@
 <?php
 
 require_once('limb/core/tests/cases/init.inc.php');
+lmb_package_require('dbal');
 
 function lmb_tests_init_db_dsn()
 {
   lmb_env_set('LIMB_CACHE_DB_META_IN_FILE', false);
 
   if(lmbToolkit::instance()->isDefaultDbDSNAvailable())
-    return;
-
-  $default_value = 'sqlite://localhost/' . lmb_env_get('LIMB_VAR_DIR') . '/sqlite_tests.db';
-  $dsn = lmb_env_get('LIMB_TEST_DB_DSN', $default_value);
-
-  echo "Using default test database '$dsn'\n";
-  lmbToolkit::instance()->setDefaultDbDSN($dsn);
+  {
+    $dsn = lmbToolkit::instance()->getDefaultDbDSN();
+    static $reported_about;
+    if(is_null($reported_about) || $reported_about != $dsn)
+    {
+      echo "INFO: Using database '{$dsn->toString()}'\n";
+      $reported_about = $dsn;
+    }    
+  }
+  else
+  {
+    $default_value = 'sqlite://localhost/' . lmb_var_dir() . '/sqlite_tests.db';
+    $dsn = lmb_env_get('LIMB_TEST_DB_DSN', $default_value);
+    lmbToolkit::instance()->setDefaultDbDSN($dsn);
+    echo "INFO: Using default test database '$dsn'\n";
+  }
 }
 
 function lmb_tests_is_db_dump_exists($prefix, $package)
@@ -22,7 +32,7 @@ function lmb_tests_is_db_dump_exists($prefix, $package)
   $skip = !file_exists($prefix.$type);
 
   if($skip)
-    echo "\n$package package tests are skipped!(no compatible database fixture found for '$type' connection)\n\n";
+    echo "INFO: $package package tests are skipped!(no compatible database fixture found for '$type' connection)\n";
 
   return $skip;
 }
@@ -33,15 +43,19 @@ function lmb_tests_setup_db($prefix)
   if(!file_exists($prefix.$type))
     return;
 
+  $file = realpath($prefix.$type);
   lmb_require('limb/dbal/src/lmbDbDump.class.php');
-
-  $dump = new lmbDbDump($prefix . $type);
+  $dump = new lmbDbDump($file);
   $dump->load();
+
+  echo "INFO: Dump is loaded from file '{$file}'\n";
 }
 
 function lmb_tests_teardown_db()
 {
-  lmb_require('limb/dbal/src/lmbDbDump.class.php');
-  $dump = new lmbDbDump(null);
-  $dump->clean();
+  $conn = lmbToolkit :: instance()->getDefaultDbConnection();
+  lmb_require('limb/dbal/src/lmbSimpleDb.class.php');
+  $db = new lmbSimpleDb($conn);
+  $db->truncateDb();
+  echo "INFO: Database was cleaned up\n";
 }
