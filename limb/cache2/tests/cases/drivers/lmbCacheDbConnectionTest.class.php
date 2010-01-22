c3af9a0a0d6d5609654d9b5e6762e4b128f589d7
korchasa <none@none>
2010-01-22 03:36:22 +0300

    [svn r8076] -- db initialization is moved to the test class
    --HG--
    branch : trunk

--- limb/cache2/tests/cases/drivers/lmbCacheDbConnectionTest.class.php
+++ limb/cache2/tests/cases/drivers/lmbCacheDbConnectionTest.class.php
@@ -6,50 +6,30 @@
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
-
 lmb_require('limb/cache2/src/drivers/lmbCacheDbConnection.class.php');
 lmb_require(dirname(__FILE__) . '/lmbCacheConnectionTest.class.php');
-
-lmb_require('limb/dbal/src/lmbDbDump.class.php');
+require_once('limb/dbal/tests/cases/init.inc.php');
 
 class lmbCacheDbConnectionTest extends lmbCacheConnectionTest
 {
   protected $storage_init_file = 'limb/dbal/common.inc.php';
 
-  function _getDumpFile()
-  {
-    $type = lmbToolkit :: instance()->getDefaultDbConnection()->getType();
-
-    $dump_file = dirname(__FILE__) . '/../../../init/cache.' . $type;
-    return file_exists($dump_file) ? $dump_file : null;
-  }
-
   function skip()
   {
-    $is_connection_exists = lmbToolkit :: instance()->isDefaultDbDSNAvailable();
-    $this->skipIf(!$is_connection_exists, 'DB connection not found. Test skiped');
-    if($is_connection_exists)
-      $this->skipIf(!$this->_getDumpFile(), 'Dump file for type "'.lmbToolkit :: instance()->getDefaultDbConnection()->getType().'" not found. Test skiped');
-  }
-
-  function setUp()
-  {
-    $this->dump = new lmbDbDump($this->_getDumpFile());
-    $this->dump->load();
-
-    parent::setUp();
-  }
-
-  function tearDown()
-  {
-    parent::tearDown();
-
-    $this->dump->clean();
+    return lmb_tests_is_db_dump_exists($this->fixture_path, 'CACHE2');
   }
 
   function __construct()
   {
     $this->dsn = 'db://dsn?table=lmb_cache2';
+    $this->fixture_path = dirname(__FILE__) . '/../../../init/cache.';
+    lmb_tests_init_db_dsn();
+    lmb_tests_setup_db($this->fixture_path);
     parent::__construct();
+  }
+
+  function __destruct()
+  {
+    lmb_tests_teardown_db();
   }
 }
