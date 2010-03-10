9fa10b152405292ab8c2920736482e2a6e4cde2b
Forumsky <none@none>
2010-03-10 17:22:07 +0300

    [svn r8146] -- added test for "default" filter when local variable not defined
    --HG--
    branch : trunk

---
+++ limb/macro/tests/cases/filters/lmbMacroDefaultFilterTest.class.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Limb PHP Framework
+ *
+ * @link http://limb-project.com
+ * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
+ */
+
+class lmbMacroDefaultFilterTest extends lmbBaseMacroTest
+{
+  function testNotDefinedLocalVariable()
+  {
+    $code = '{$var|default:"val"}';
+    $tpl = $this->_createMacroTemplate($code, 'tpl.html');
+    $out = $tpl->render();
+    $this->assertEqual($out, 'val');
+  }
+}
+
