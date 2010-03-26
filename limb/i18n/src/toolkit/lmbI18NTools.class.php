8a540d61d2f0635a2fbe4ace14342186060d9a1c
Forumsky <none@none>
2010-03-26 10:58:18 +0300

    [svn r8150] -- accesing public methods from mixed toolkit context to make ability be overriden
    --HG--
    branch : trunk

--- limb/i18n/src/toolkit/lmbI18NTools.class.php
+++ limb/i18n/src/toolkit/lmbI18NTools.class.php
@@ -16,7 +16,7 @@ lmb_env_setor('LIMB_LOCALE_INCLUDE_PATH', 'i18n/locale;limb/i18n/i18n/locale');
  * class lmbI18NTools.
  *
  * @package i18n
- * @version $Id: lmbI18NTools.class.php 7676 2009-03-03 22:37:28Z korchasa $
+ * @version $Id: lmbI18NTools.class.php 8150 2010-03-26 07:58:18Z Forumsky $
  */
 class lmbI18NTools extends lmbAbstractTools
 {
@@ -61,10 +61,10 @@ class lmbI18NTools extends lmbAbstractTools
   function getLocaleObject($locale = null)
   {
     if(!$locale)
-      $locale = $this->getLocale();
+      $locale = $this->toolkit->getLocale();
 
     if(!isset($this->locale_objects[$locale]))
-      $this->locale_objects[$locale] = $this->createLocaleObject($locale);
+      $this->locale_objects[$locale] = $this->toolkit->createLocaleObject($locale);
 
     return $this->locale_objects[$locale];
   }
@@ -91,7 +91,7 @@ class lmbI18NTools extends lmbAbstractTools
   {
     if(!isset($this->dictionaries[$locale . '@' . $domain]))
     {
-      $backend = $this->getDictionaryBackend();
+      $backend = $this->toolkit->getDictionaryBackend();
       $this->dictionaries[$locale . '@' . $domain] = $backend->load($locale, $domain);
     }
 
@@ -105,7 +105,7 @@ class lmbI18NTools extends lmbAbstractTools
 
   function translate($text, $arg1 = null, $arg2 = null)
   {
-    $locale = $this->getLocale();
+    $locale = $this->toolkit->getLocale();
 
     $domain = 'default';
     $attributes = null;
@@ -119,7 +119,7 @@ class lmbI18NTools extends lmbAbstractTools
     elseif(is_string($arg1))
       $domain = $arg1;
 
-    if($dict = $this->getDictionary($locale, $domain))
+    if($dict = $this->toolkit->getDictionary($locale, $domain))
       return $dict->translate($text, $attributes);
     else
       return $text;
