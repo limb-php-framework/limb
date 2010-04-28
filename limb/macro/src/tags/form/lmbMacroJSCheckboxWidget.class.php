71fb57c780b69cb2535fe8f2bbd8dd7a46c7ce94
conf <none@none>
2010-04-28 13:23:51 +0400

    [svn r8185] -- checkbox id now conforms W3C standards
    --HG--
    branch : trunk

--- limb/macro/src/tags/form/lmbMacroJSCheckboxWidget.class.php
+++ limb/macro/src/tags/form/lmbMacroJSCheckboxWidget.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 
 lmb_require('limb/macro/src/tags/form/lmbMacroCheckableInputWidget.class.php');
@@ -19,24 +19,24 @@ class lmbMacroJSCheckboxWidget extends lmbMacroCheckableInputWidget
 {
   protected $skip_render = array('name', 'checked_value');
   protected $hidden_id;
-    
+
   function renderAttributes()
   {
     parent :: renderAttributes();
 
-    $this->hidden_id = (string) microtime().mt_rand();
+    $this->hidden_id = uniqid('js_checkbox_');
     echo " onchange=\"this.form.elements['{$this->hidden_id}'].value = 1*this.checked\"";
   }
 
   function renderHidden()
   {
     $hidden_name = $this->getAttribute('name');
-    
+
     if($this->isChecked())
       $value = 1;
     else
       $value = 0;
-      
+
     echo "<input type=\"hidden\" id=\"{$this->hidden_id}\" name=\"{$hidden_name}\" value=\"{$value}\" />";
   }
 }
--- limb/macro/tests/cases/tags/form/lmbMacroJsCheckboxTagTest.class.php
+++ limb/macro/tests/cases/tags/form/lmbMacroJsCheckboxTagTest.class.php
@@ -13,13 +13,13 @@ class lmbMacroJsCheckboxTagTest extends lmbBaseMacroTest
   {
     $template = '{{js_checkbox name="my_checkbox" value="$#var"/}}';
 
-    $page = $this->_createMacroTemplate($template, 'tpl.html'); 
+    $page = $this->_createMacroTemplate($template, 'tpl.html');
     $page->set('var', 1);
 
     $html = new SimpleXMLElement('<foo>'.$page->render().'</foo>');
 
     $this->assertEqual($html->input[0]['type'], 'checkbox');
-    
+
     $this->assertEqual($html->input[1]['type'], 'hidden');
     $this->assertEqual($html->input[1]['name'], 'my_checkbox');
     $this->assertEqual($html->input[1]['value'], '0');
@@ -29,42 +29,54 @@ class lmbMacroJsCheckboxTagTest extends lmbBaseMacroTest
   {
     $template = '{{js_checkbox name="my_checkbox" value="$#var" checked="checked"/}}';
 
-    $page = $this->_createMacroTemplate($template, 'tpl.html'); 
-    $page->set('var', 1);     
-    
+    $page = $this->_createMacroTemplate($template, 'tpl.html');
+    $page->set('var', 1);
+
     $html = new SimpleXMLElement('<foo>'.$page->render().'</foo>');
 
     $this->assertEqual($html->input[1]['type'], 'hidden');
-    $this->assertEqual($html->input[1]['value'], 1);       
+    $this->assertEqual($html->input[1]['value'], 1);
   }
-  
+
   function testChecked_With_CheckedValueAttribute()
   {
     $template = '{{js_checkbox name="my_checkbox" checked_value="$#var" checked="checked"/}}';
 
-    $page = $this->_createMacroTemplate($template, 'tpl.html'); 
-    $page->set('var', 1);     
-    
+    $page = $this->_createMacroTemplate($template, 'tpl.html');
+    $page->set('var', 1);
+
     $html = new SimpleXMLElement('<foo>'.$page->render().'</foo>');
 
     $this->assertEqual($html->input[0]['checked'], 'checked');
-    $this->assertEqual($html->input[1]['value'], 1);        
+    $this->assertEqual($html->input[1]['value'], 1);
   }
 
   function testNotChecked_With_CheckedValueAttribute_And_ValueAttribute()
   {
     $template = '{{js_checkbox name="my_checkbox" checked_value="$#var" value="1" checked="checked"/}}';
 
-    $page = $this->_createMacroTemplate($template, 'tpl.html'); 
+    $page = $this->_createMacroTemplate($template, 'tpl.html');
     $page->set('var', 2);
 
     $html = new SimpleXMLElement('<foo>'.$page->render().'</foo>');
 
     $this->assertEqual($html->input[0]['type'], 'checkbox');
     $this->assertEqual($html->input[0]['value'], 1);
-    
+
     $this->assertEqual($html->input[1]['type'], 'hidden');
-    $this->assertEqual($html->input[1]['value'], 0);    
+    $this->assertEqual($html->input[1]['value'], 0);
+  }
+
+  function testIdConformsW3C()
+  {
+    $template = '{{js_checkbox name="my_checkbox" value="$#var" checked="checked"/}}';
+
+    $page = $this->_createMacroTemplate($template, 'tpl.html');
+    $page->set('var', 1);
+
+    $html = new SimpleXMLElement('<foo>'.$page->render().'</foo>');
+    $error_message = 'Id must start from letter, that must be followed by letters, digits, underscores, colons and dots';
+    $this->assertPattern('~[a-z][a-z\d_:.]~i', $html->input[1]['id'], $error_message);
   }
 }
 
