6c3e3979c6779f5d19c19f43604d12085f94baa7
Stanislav Korchagin <korchasa@gmail.com>
2010-05-19 05:17:05 +0400

    -- Fixed regexp for numeric format in lmbNumericValueRangeRule
    --HG--
    branch : RC-prepare

--- limb/validation/src/rule/lmbNumericValueRangeRule.class.php
+++ limb/validation/src/rule/lmbNumericValueRangeRule.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 lmb_require('limb/validation/src/rule/lmbSingleFieldRule.class.php');
 
@@ -45,7 +45,7 @@ class lmbNumericValueRangeRule extends lmbSingleFieldRule
 
   function check($value)
   {
-    if (!preg_match('/^[+-]?(\d*)$/', $value, $match))
+    if (!preg_match('/^[+-]?(\d*)\.?(\d*)$/', $value, $match))
       $this->error('{Field} must be a valid number.');
 
     if($value < $this->min_value)
--- limb/validation/tests/cases/rule/lmbNumericValueRangeRuleTest.class.php
+++ limb/validation/tests/cases/rule/lmbNumericValueRangeRuleTest.class.php
@@ -2,14 +2,26 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 lmb_require('limb/validation/src/rule/lmbNumericValueRangeRule.class.php');
 
 class lmbNumericValueRangeRuleTest extends lmbValidationRuleTestCase
 {
+  function testFormat()
+  {
+    $rule = new lmbNumericValueRangeRule('testfield', 0, 5);
+
+    $dataspace = new lmbSet();
+    $dataspace->set('testfield', '1.01');
+
+    $this->error_list->expectNever('addError');
+
+    $rule->validate($dataspace, $this->error_list);
+  }
+
   function testInRange()
   {
     $rule = new lmbNumericValueRangeRule('testfield', 0, 5);
@@ -30,7 +42,7 @@ class lmbNumericValueRangeRuleTest extends lmbValidationRuleTestCase
     $dataspace->set('testfield', -10);
 
     $this->error_list->expectOnce('addError',
-                                  array(lmb_i18n('{Field} must be not less than {value}.', 'validation'),
+                                  array('{Field} must be not less than {value}.',
                                         array('Field' => 'testfield'),
                                         array('value' => 1)));
 
@@ -45,7 +57,7 @@ class lmbNumericValueRangeRuleTest extends lmbValidationRuleTestCase
     $dataspace->set('testfield', 10);
 
     $this->error_list->expectOnce('addError',
-                                  array(lmb_i18n('{Field} must be not greater than {value}.', 'validation'),
+                                  array('{Field} must be not greater than {value}.',
                                         array('Field' => 'testfield'),
                                         array('value' => 5)));
 
@@ -60,7 +72,7 @@ class lmbNumericValueRangeRuleTest extends lmbValidationRuleTestCase
     $dataspace->set('testfield', '4fdfasd');
 
     $this->error_list->expectOnce('addError',
-                                  array(lmb_i18n('{Field} must be a valid number.', 'validation'),
+                                  array('{Field} must be a valid number.',
                                         array('Field'=>'testfield'),
                                         array()));
 
