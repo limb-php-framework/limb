51c3428c342b53a227fb74c613b55248cc27e792
Stanislav Korchagin <korchasa@gmail.com>
2010-05-25 04:30:22 +0400

    -- Fixed missed lmb_require
    --HG--
    branch : RC-prepare

--- limb/cache2/src/wrappers/lmbLoggedCacheWrapper.class.php
+++ limb/cache2/src/wrappers/lmbLoggedCacheWrapper.class.php
@@ -6,7 +6,7 @@
  * @copyright  Copyright &copy; 2004-2007 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
-lmb_require('limb/cache2/src/lmbCacheBaseWrapper.class.php');
+lmb_require('limb/cache2/src/wrappers/lmbCacheBaseWrapper.class.php');
 lmb_require('limb/cache2/src/lmbCacheLog.class.php');
 lmb_require('limb/core/src/lmbBacktrace.class.php');
 lmb_require('limb/dbal/src/query/lmbInsertQuery.class.php');
