1b523c10bdf794afab122f336f1f9ff75202d510
Stanislav Korchagin <korchasa@gmail.com>
2010-05-26 15:51:22 +0400

    -- Fixed fixture function name
    --HG--
    branch : RC-prepare

--- incubator/limb/cron/tests/cases/.skipif.php
+++ incubator/limb/cron/tests/cases/.skipif.php
@@ -4,4 +4,4 @@ lmb_require('limb/dbal/tests/cases/init.inc.php');
 
 lmb_tests_init_db_dsn();
 
-return lmb_tests_is_db_dump_exists(dirname(__FILE__).'/../../init/db.', 'CRON');
+return lmb_tests_db_dump_does_not_exist(dirname(__FILE__).'/../../init/db.', 'CRON');
