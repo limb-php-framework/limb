78a7c0871da3e06dc05797994e83bc11ab3f25c5
Stanislav Korchagin <korchasa@gmail.com>
2010-06-02 13:08:39 +0400

    -- Fixed common.inc.php instructions order
    --HG--
    branch : RC-prepare

--- limb/cms/common.inc.php
+++ limb/cms/common.inc.php
@@ -12,10 +12,11 @@
  * @version $Id: common.inc.php 8048 2010-01-19 22:12:02Z korchasa $
  */
 require_once('limb/core/common.inc.php');
-lmb_package_require('web_app');
-lmb_require('limb/cms/toolkit.inc.php');
 
 lmb_env_setor('LIMB_CONTROLLERS_INCLUDE_PATH', 'src/controller;limb/cms/src/controller;limb/web_app/src/controller');
+
+lmb_package_require('web_app');
+lmb_require('limb/cms/toolkit.inc.php');
 
 lmb_env_setor('JQUERY_FILE_URL','/shared/js/js/jquery/v1.2.3.js');
 lmb_env_setor('CMS_STATIC_FILES_VERSION', '2');
