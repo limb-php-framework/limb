aee1de2955cd7b6ad2cbc93c93d44ae2b7d4e2ee
Stanislav Korchagin <korchasa@gmail.com>
2010-06-02 13:09:26 +0400

    -- Added type check in admin navigation fetcher
    --HG--
    branch : RC-prepare

--- limb/cms/src/fetcher/lmbCmsAdminNavigationFetcher.class.php
+++ limb/cms/src/fetcher/lmbCmsAdminNavigationFetcher.class.php
@@ -1,6 +1,7 @@
 <?php
-lmb_require('limb/web_app/src/fetcher/lmbFetcher.class.php');
 lmb_require('limb/core/src/lmbCollection.class.php');
+lmb_require('limb/web_app/src/fetcher/lmbFetcher.class.php');
+lmb_require('limb/cms/src/exception/lmbCmsException.class.php');
 
 class lmbCmsAdminNavigationFetcher extends lmbFetcher
 {
@@ -9,7 +10,10 @@ class lmbCmsAdminNavigationFetcher extends lmbFetcher
     $toolkit = lmbToolkit :: instance();
     $conf = $toolkit->getConf('navigation');
 
-    $data = $conf->get($toolkit->getCmsUser()->getRoleType());
+    $role = $toolkit->getCmsUser()->getRoleType();
+    lmb_assert_true($conf->has($role), "Navigation section for current user role not found");
+    $data = $conf->get($role);
+
     if(is_array($data))
       return new lmbCollection($data);
     else
