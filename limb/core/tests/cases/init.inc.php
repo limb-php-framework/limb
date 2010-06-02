1ce0036f15c7deaabffbe5be236265578678c3f8
Stanislav Korchagin <korchasa@gmail.com>
2010-06-03 03:49:42 +0400

    -- Added lmb_tests_cleanup_var_dir()
    --HG--
    branch : RC-prepare

--- limb/core/tests/cases/init.inc.php
+++ limb/core/tests/cases/init.inc.php
@@ -10,3 +10,10 @@ function lmb_tests_init_var_dir($value)
   lmb_var_dir($real);
   echo "INFO: Var dir inited in {$real}\n";
 }
+
+function lmb_tests_cleanup_var_dir()
+{
+  $var_dir = lmb_var_dir();
+  lmbFs::rm($var_dir);
+  echo "INFO: Var dir cleaned up in {$var_dir}\n";
+}
