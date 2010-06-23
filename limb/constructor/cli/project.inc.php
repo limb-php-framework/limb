eb89057f9a174bb4465d91539f3bb2097f548191
mihailoff@gmail.com <none@none>
2010-06-23 13:59:22 +0300

    -- update of setup.php witch generating in cms_init
    --HG--
    branch : RC-prepare

--- limb/constructor/cli/project.inc.php
+++ limb/constructor/cli/project.inc.php
@@ -73,15 +73,23 @@ EOD;
   $setup = <<<EOD
 <?php
 
-set_include_path(dirname(__FILE__) . PATH_SEPARATOR . dirname(__FILE__) . '/lib/');
+set_include_path(implode(PATH_SEPARATOR,
+  array(
+    dirname(__FILE__) . '/lib/',
+    dirname(__FILE__),
+    get_include_path()
+  )
+));
+
+require_once('limb/core/common.inc.php');
 
 if(file_exists(dirname(__FILE__) . '/setup.override.php'))
   require_once(dirname(__FILE__) . '/setup.override.php');
 
-require_once('limb/core/common.inc.php');
-require_once('limb/cms/common.inc.php');
+lmb_package_require('cms');
 
 lmb_env_setor('LIMB_VAR_DIR', dirname(__FILE__) . '/var/');
+lmb_env_setor('LIMB_APP_MODE' , 'production');
 EOD;
 
   $root = taskman_prop('PROJECT_DIR');
