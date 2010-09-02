592d55a0fea56156a10e52211d7313f11084a24d
korchasa <korchasa@gmail.com>
2010-09-02 09:46:55 +0400

    Added postfix for clip tag

--- macro/src/filters/clip.filter.php
+++ macro/src/filters/clip.filter.php
@@ -5,8 +5,8 @@
  * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
- */ 
- 
+ */
+
 /**
  * class lmbMacroClipFilter
  * Clipping the string by given lenght. Multibyte unsafe
@@ -14,11 +14,21 @@
  * @filter clip
  * @package macro
  * @version $Id$
- */ 
+ */
 class lmbMacroClipFilter extends lmbMacroFilter
 {
+	const DEFAULT_CLIP_LIMIT = 255;
+
   function getValue()
   {
-    return 'substr('.$this->base->getValue().', 0, '.$this->params[0].')';
-  }  
-} 
+  	$value_ptr = $this->base->getValue();
+  	$limit = isset($this->params[0]) ? $this->params[0] : self::DEFAULT_CLIP_LIMIT;
+  	$end_symbol = isset($this->params[1]) ? $this->params[1] : '';
+
+    $substr = 'substr('.$value_ptr.', 0, '.$this->params[0].')';
+    if ($end_symbol)
+      $substr .= '.((strlen('.$value_ptr.') > '.$limit.') ? '.$end_symbol.' : "")';
+
+    return $substr;
+  }
+}
