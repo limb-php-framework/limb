94c1264da120763a9244137ca1b8c883c9aadde0
Shein Alexey <confik@gmail.com>
2010-09-14 15:34:58 +0500

    -- fixed test breakage on logging exceptions

--- validation/tests/cases/lmbValidationExceptionTest.class.php
+++ validation/tests/cases/lmbValidationExceptionTest.class.php
@@ -2,9 +2,9 @@
 /*
  * Limb PHP Framework
  *
- * @link http://limb-project.com 
+ * @link http://limb-project.com
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
- * @license    LGPL http://www.gnu.org/copyleft/lesser.html 
+ * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
 lmb_require('limb/validation/src/lmbErrorList.class.php');
 lmb_require('limb/validation/src/exception/lmbValidationException.class.php');
@@ -17,7 +17,7 @@ class lmbValidationExceptionTest extends UnitTestCase
     $error_list->addError('error1');
     $error_list->addError('error2');
     $exception = new lmbValidationException('Message.', $error_list, $params = array());
-    $this->assertEqual($exception->getMessage(), 'Message. Errors list : error1, error2');
+    $this->assertEqual($exception->getOriginalMessage(), 'Message. Errors list : error1, error2');
   }
 }
 
