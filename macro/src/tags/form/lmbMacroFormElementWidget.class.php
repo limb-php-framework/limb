a53abe7a629fbf53268b70ffdc0d9af29fa54170
korchasa <korchasa@gmail.com>
2010-09-24 18:55:37 +0400

    -- Added base test class for tags tests -- Added test for lmbMacroInputWidget :: renderAttributes() -- Name attribute now have the same value as id by default #LMB-5

--- macro/src/tags/form/lmbMacroFormElementWidget.class.php
+++ macro/src/tags/form/lmbMacroFormElementWidget.class.php
@@ -6,12 +6,12 @@
  * @copyright  Copyright &copy; 2004-2009 BIT(http://bit-creative.com)
  * @license    LGPL http://www.gnu.org/copyleft/lesser.html
  */
- 
+
 lmb_require('limb/macro/src/compiler/lmbMacroHtmlTagWidget.class.php');
 
 /**
  * class lmbMacroFormElementWidget.
- * Base class for any form fields object at runtime 
+ * Base class for any form fields object at runtime
  *
  * @package macro
  * @version $Id$
@@ -20,63 +20,63 @@ class lmbMacroFormElementWidget extends lmbMacroHtmlTagWidget
 {
   protected $has_errors = false;
   protected $form;
-  
+
   function __construct($id)
   {
     parent :: __construct($id);
-    
+
     $this->skip_render = array_merge($this->skip_render, array('error_class', 'error_style'));
   }
-  
+
   function getDisplayName()
   {
     if($this->hasAttribute('title'))
       return $this->getAttribute('title');
     if($this->hasAttribute('alt'))
       return $this->getAttribute('alt');
-    
+
     return $this->runtime_id;
   }
-  
+
   function setForm($form)
   {
     $this->form = $form;
   }
-  
+
   function setErrorState($has_errors = true)
   {
     $this->has_errors = $has_errors;
-    
+
     if($has_errors)
     {
-      if($this->hasAttribute('error_class')) 
+      if($this->hasAttribute('error_class'))
         $this->setAttribute('class', $this->getAttribute('error_class'));
-      if($this->hasAttribute('error_style')) 
+      if($this->hasAttribute('error_style'))
        $this->setAttribute('style', $this->getAttribute('error_style'));
     }
   }
-  
+
   function hasErrors()
   {
     return $this->has_errors;
   }
-  
+
   function getValue()
   {
     if($this->hasAttribute('value'))
       return $this->getAttribute('value');
-    
+
     return $this->_getValueFromFormDatasource();
-  }   
-  
+  }
+
   function getName()
   {
     if($this->hasAttribute('name'))
       return $this->getAttribute('name');
 
     return $this->getRuntimeId();
-  }   
-  
+  }
+
   protected function _getValueFromFormDatasource()
   {
     if(is_object($this->form))
@@ -90,6 +90,9 @@ class lmbMacroFormElementWidget extends lmbMacroHtmlTagWidget
 
   function renderAttributes()
   {
+  	if (!$this->hasAttribute('name') && $this->hasAttribute('id'))
+  	  $this->setAttribute('name', $this->getAttribute('id'));
+
     foreach(array('readonly', 'disabled') as $attribute)
     {
       if(!$this->getBoolAttribute($attribute, false))
