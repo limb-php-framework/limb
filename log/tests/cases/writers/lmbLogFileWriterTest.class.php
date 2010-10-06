6b75c905a9f1e60d7eb14b8827f82b07b5b128b7
knevcher <knevcher@gmail.com>
2010-10-06 15:03:54 +0800

    -- added additional test for lmbLogFileWriter

--- log/tests/cases/writers/lmbLogFileWriterTest.class.php
+++ log/tests/cases/writers/lmbLogFileWriterTest.class.php
@@ -23,4 +23,28 @@ class lmbLogFileWriterTest extends UnitTestCase
     $this->assertPattern('/Error/', $content);
     $this->assertPattern('/foo/', $content);
   }
+
+  function testWrite_SetEntryTitle()
+  {
+    $toolkit = lmbToolkit::instance();
+    $toolkit->setLogEntryTitle('some_title');
+
+    $dsn = new lmbUri('file://'.lmb_var_dir().'/logs/info'.uniqid().'.log');
+
+    $conf = array(
+      'logs' => array('foo' => $dsn)
+    );
+
+    $toolkit->setConf('log', $conf);
+    $toolkit->log('message', $level = LOG_INFO, $params = array(), null, $log_name = 'foo');
+
+    $writers = $toolkit->getLog('foo')->getWriters();
+    $this->assertIsA($writers[0], 'lmbLogFileWriter');
+
+    $content = file_get_contents($writers[0]->getLogFile());
+
+    $this->assertPattern('/some_title/', $content);
+    $this->assertPattern('/Info/', $content);
+    $this->assertPattern('/message/', $content);
+  }
 }
