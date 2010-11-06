8d9508bfccaddc5e6ba5c978b5b58571b9ce5c5a
korchasa <korchasa@gmail.com>
2010-11-06 09:24:54 +0300

    PHPDoc

--- wysiwyg/src/macro/lmbMacroCKEditorWidget.class.php
+++ wysiwyg/src/macro/lmbMacroCKEditorWidget.class.php
@@ -3,7 +3,7 @@
 * Limb PHP Framework
 *
 * @link http://limb-project.com
-* @copyright  Copyright © 2004-2009 BIT(http://bit-creative.com)
+* @copyright  Copyright &copy 2004-2012 BIT(http://bit-creative.com)
 * @license    LGPL http://www.gnu.org/copyleft/lesser.html
 */
 
