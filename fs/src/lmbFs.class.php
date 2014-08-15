84406e575d9ecd55629ae924dbe32d67ded1f280
Mikhail Maltcev <m.matlcev@eastwoodlab.ru>
2014-08-15 16:30:09 +0400

    fixed bug (lmbFs)

--- fs/src/lmbFs.class.php
+++ fs/src/lmbFs.class.php
@@ -421,7 +421,7 @@ class lmbFs
 
   static function isPathRelative($path, $fs_type = self :: LOCAL)
   {
-    return !self :: isPathAbsolute($path, $os_type);
+    return !self :: isPathAbsolute($path, $fs_type);
   }
 
   static function isPathAbsolute($path, $fs_type = self :: LOCAL)
