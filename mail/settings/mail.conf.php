acb1a0d0ff775b57560f3e70b56e595cf47ed879
Alexander Morozov <numelion@gmail.com>
2014-09-22 12:22:34 +0400

    Added new version and SSL encryption for PHPMailer

--- mail/settings/mail.conf.php
+++ mail/settings/mail.conf.php
@@ -1,13 +1,14 @@
 <?php
 
 $conf = array(
-
-  'use_phpmail' 		=> lmb_env_get('LIMB_USE_PHPMAIL', false),
-  'smtp_host' 			=> lmb_env_get('LIMB_SMTP_HOST', 'localhost'),
-  'smtp_port' 			=> lmb_env_get('LIMB_SMTP_PORT', '25'),
-  'smtp_auth' 			=> lmb_env_get('LIMB_SMTP_AUTH', false),
-  'smtp_user' 			=> lmb_env_get('LIMB_SMTP_USER', ''),
-  'smtp_password' 		=> lmb_env_get('LIMB_SMTP_PASSWORD', ''),
-  'sender'				=> 'set-me-in-mail-conf@limb-project.com',
-  'macro_template_parser' => 'mailpart'
+  'phpmailer_version_name' => lmb_env_get('PHPMAILER_VERSION_NAME', 'phpmailer-5.2.8'),
+  'use_phpmail'            => lmb_env_get('LIMB_USE_PHPMAIL', false),
+  'smtp_host'              => lmb_env_get('LIMB_SMTP_HOST', 'localhost'),
+  'smtp_port'              => lmb_env_get('LIMB_SMTP_PORT', '25'),
+  'smtp_auth'              => lmb_env_get('LIMB_SMTP_AUTH', false),
+  'smtp_user'              => lmb_env_get('LIMB_SMTP_USER', ''),
+  'smtp_password'          => lmb_env_get('LIMB_SMTP_PASSWORD', ''),
+  'smtp_secure'            => lmb_env_get('LIMB_SMTP_SECURE', ''),  /* Options: "", "ssl" or "tls" */
+  'sender'                 => 'set-me-in-mail-conf@limb-project.com',
+  'macro_template_parser'  => 'mailpart'
 );
